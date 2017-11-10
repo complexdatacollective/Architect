7827ddf50cd37d700c38939f0888439f5447ab2a
Steve Mckellar <steve@wwqrd.uk>
2017-11-10 12:18:46 +0000

    Remove card transition component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as CardTransition } from './CardTransition';
 export { default as ScreenTransition } from './ScreenTransition';
 export { default as Timeline } from './Timeline';
 export { default as TimelineAddNew } from './TimelineAddNew';
