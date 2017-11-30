af57c9915be22cd0046e0d080af49820c85643fe
Steve Mckellar <steve@wwqrd.uk>
2017-11-30 14:37:25 +0000

    Fix missing dev config for module shims

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -91,6 +91,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'fs': `${paths.appSrc}/utils/fs-shim`,
+      'electron': `${paths.appSrc}/utils/electron-shim`,
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
