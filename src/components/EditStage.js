cac2606b5cabd04c3268200d4c23bb290589bd1d
Steve Mckellar <steve@wwqrd.uk>
2017-11-30 15:23:54 +0000

    Mocked edit stage subcomponents

--- src/components/EditStage.js
+++ src/components/EditStage.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const StageTitle = () => null;
+const StageNodeType = () => null;
+const StageForm = () => null;
+const StagePrompts = () => null;
+const StagePanels = () => null;
+
 const EditStage = () => (
   <div className="edit-stage">
     <div className="edit-stage__section">
-      This is an editable stage
+      <StageTitle />
+      <StageNodeType />
+      <StageForm />
+      <StagePrompts />
+      <StagePanels />
     </div>
     <div className="edit-skip-logic__guidance">
       This is some guidance about that section.
