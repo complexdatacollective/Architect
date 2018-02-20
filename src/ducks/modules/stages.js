c669d2ebff51504d5948ce2470638fb1f2e77d57
Steve Mckellar <steve@wwqrd.uk>
2018-02-20 12:11:05 +0000

    Change default protocol

--- src/ducks/modules/stages.js
+++ src/ducks/modules/stages.js
@@ -3,7 +3,10 @@ import { map, max } from 'lodash';
 const ADD_STAGE = Symbol('STAGES/ADD_STAGE');
 const UPDATE_STAGE = Symbol('STAGES/UPDATE_STAGE');
 
-const initialState = [{ id: 1, type: 'NameGenerator' }, { id: 2, type: 'Sociogram' }];
+const initialState = [
+  { id: 1, type: 'Information' },
+  { id: 1, type: 'NameGenerator' },
+];
 
 const maxId = state => max(map(state, 'id'));
 const nextId = state => ((maxId(state) || 0) + 1);
