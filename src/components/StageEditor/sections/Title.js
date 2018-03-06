c8c7d4d95c997ccac313e7a5819451ef23379c4f
Steve Mckellar <steve@wwqrd.uk>
2018-03-06 16:49:58 +0000

    Use TextInput for title for the moment

--- src/components/StageEditor/sections/Title.js
+++ src/components/StageEditor/sections/Title.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { SeamlessTextInput } from '../../../components/Form';
+import { TextInput } from '../../../components/Form';
 
 const Title = ({ stage: { title }, onChange }) => ([
   <div className="stage-editor__section" key="edit">
-    <SeamlessTextInput value={title} onChange={newTitle => onChange({ title: newTitle })} />
+    <TextInput value={title} onChange={newTitle => onChange({ title: newTitle })} />
   </div>,
   <div className="stage-editor__guidance" key="guidance">
     What is the title for this interface?
