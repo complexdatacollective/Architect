89b79594c28005cbc6b54f958c5889c9f374260d
Steve Mckellar <steve@wwqrd.uk>
2018-03-07 17:58:55 +0000

    Interactive guidance feature WIP

--- src/components/StageEditor/StageEditor.js
+++ src/components/StageEditor/StageEditor.js
@@ -1,9 +1,65 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   Title,
   ContentItems,
 } from './sections';
+
+class Guided extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: 1,
+    };
+  }
+
+  onMouseOver = (index) =>
+    this.setState({ active: index });
+
+  render() {
+    const isActive = this.state.isActive;
+
+    return React.Children.toArray(this.props.children)
+      .map((child, index) => {
+        const isActive = this.state.active === index;
+        return React.cloneElement(
+          child,
+          {
+            isActive,
+            onMouseOver: () => this.onMouseOver(index)
+          }
+        );
+      });
+  }
+}
+
+const Section = ({ children, isActive, onMouseOver }) => (
+  <div onMouseOver={onMouseOver}>
+    {
+      React.Children.toArray(children)
+        .map((child) =>
+          React.cloneElement(child, { isActive }),
+        )
+    }
+  </div>
+);
+
+const Edit = ({ isActive, children }) => (
+  <div>
+    {isActive && 'Yes' }
+    {children}
+  </div>
+);
+
+const Guidance = ({ isActive, children }) => (
+  <div>
+    {isActive && 'Yes' }
+    {children}
+  </div>
+);
 
 /*
  * Renders the relevant interface editor
@@ -30,6 +86,26 @@ const StageEditor = (props) => {
 
   return (
     <div className="stage-editor">
+      <Guided>
+        <Section>
+          <Edit>
+            Editor goes here
+          </Edit>
+
+          <Guidance>
+            Some content here.
+          </Guidance>
+        </Section>
+        <Section>
+          <Edit>
+            Editor goes here
+          </Edit>
+
+          <Guidance>
+            Some content here.
+          </Guidance>
+        </Section>
+      </Guided>
       <div className="stage-editor-section">
         <div className="stage-editor-section__edit">
           <h1>{props.stage.type}</h1>
