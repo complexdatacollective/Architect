54a9970c27a1b090901014a13cffcd765a15ebf6
Steve Mckellar <steve@wwqrd.uk>
2018-03-09 14:23:30 +0000

    Fix the instructions interface output to match protocol spec

--- src/components/StageEditor/sections/Title.js
+++ src/components/StageEditor/sections/Title.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { Section, Edit, Guidance } from '../../Guided';
 import { SeamlessTextInput } from '../../../components/Form';
 
-const Title = ({ stage: { title }, onChange, ...props }) => (
+const Title = ({ stage: { label }, onChange, ...props }) => (
   <Section className="stage-editor-section" {...props}>
     <Edit className="stage-editor-section__edit">
       <h2>Title</h2>
       <SeamlessTextInput
-        value={title}
+        value={label}
         placeholder="Enter your title here"
         className="stage-editor-section-title"
-        onChange={newTitle => onChange({ title: newTitle })}
+        onChange={newLabel => onChange({ label: newLabel })}
       />
     </Edit>
     <Guidance className="stage-editor-section__guidance">
