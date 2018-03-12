3eca858e26c050a01f6485f05a2d9764a9da7b92
Steve Mckellar <steve@wwqrd.uk>
2018-03-12 15:33:46 +0000

    Fix stages reducer test

--- src/ducks/modules/protocol/__tests__/stages.test.js
+++ src/ducks/modules/protocol/__tests__/stages.test.js
@@ -10,12 +10,12 @@ describe('store.stages', () => {
       [],
       actionCreators.addStage(newStage, 0),
     );
-    expect(addStageToBlankState[0]).toEqual({ ...newStage, id: 1 });
+    expect(addStageToBlankState[0]).toMatchObject({ ...newStage });
 
     const addStageToFilledState = reducer(
       [{ id: 3 }, { id: 9 }, { id: 5 }],
       actionCreators.addStage(newStage, 2),
     );
-    expect(addStageToFilledState[2]).toEqual({ ...newStage, id: 10 });
+    expect(addStageToFilledState[2]).toMatchObject({ ...newStage });
   });
 });
