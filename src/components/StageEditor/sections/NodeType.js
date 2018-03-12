5821fb02448d3dcdd4107b778541f62fecc22e10
Steve Mckellar <steve@wwqrd.uk>
2018-03-12 17:32:06 +0000

    Linted NodeType editor

--- src/components/StageEditor/sections/NodeType.js
+++ src/components/StageEditor/sections/NodeType.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -7,13 +5,14 @@ import { keys } from 'lodash';
 import { Section, Edit, Guidance } from '../../Guided';
 import { OptionsInput } from '../../../components/Form';
 
+// eslint-disable-next-line
 const NodeTypeOption = ({ selected, value }) => (
   <div className="edit-stage-node-type__option">
     {value} {selected && ' (selected)'}
   </div>
 );
 
-const NodeType = ({ stage: { nodeType }, nodeTypes, onChange, dispatch,...props }) => (
+const NodeType = ({ stage: { nodeType }, nodeTypes, onChange, dispatch, ...props }) => (
   <Section className="stage-editor-section" {...props}>
     <Edit className="stage-editor-section__edit">
       <h2>Node Type</h2>
@@ -22,7 +21,7 @@ const NodeType = ({ stage: { nodeType }, nodeTypes, onChange, dispatch,...props 
         options={nodeTypes}
         component={NodeTypeOption}
         value={nodeType}
-        onChange={(value) => onChange({ nodeType: value })}
+        onChange={value => onChange({ nodeType: value })}
       />
     </Edit>
     <Guidance className="stage-editor-section__guidance">
@@ -33,15 +32,18 @@ const NodeType = ({ stage: { nodeType }, nodeTypes, onChange, dispatch,...props 
 
 NodeType.propTypes = {
   stage: PropTypes.object,
+  nodeTypes: PropTypes.arrayOf(PropTypes.string),
   onChange: PropTypes.func,
+  dispatch: PropTypes.func.isRequired,
 };
 
 NodeType.defaultProps = {
   stage: {},
+  nodeTypes: [],
   onChange: () => {},
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   nodeTypes: keys(state.protocol.present.variableRegistry.node),
 });
 
