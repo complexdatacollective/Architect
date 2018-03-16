b1ffda068d651d591ddcea3d972b162ffc849351
Steve Mckellar <steve@wwqrd.uk>
2018-03-16 10:16:57 +0000

    Generalise sortable for different prompt types

--- src/components/StageEditor/sections/Prompts.js
+++ src/components/StageEditor/sections/Prompts.js
@@ -1,15 +1,40 @@
 /* eslint-disable */
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import { has } from 'lodash';
 import { Section, Edit, Guidance } from '../../Guided';
+import SortableItems from './SortableItems';
+import Prompt from './Prompt';
 
-const Prompts = ({ stage, onChange, ...props }) => (
-  <Section className="stage-editor-section" {...props}>
-    <Edit className="stage-editor-section__edit">
-      <h2></h2>
-    </Edit>
-    <Guidance className="stage-editor-section__guidance" />
-  </Section>
-);
+const dummyItems = [{ text: 'test a' }, { text: 'test b' }, { text: 'test c' }];
 
-export default Prompts;
+const Prompts = ({ stage: { prompts }, show, onChange, ...props }) => {
+  const items = prompts || dummyItems;
+  return (
+    <Section className="stage-editor-section" {...props}>
+      <Edit className="stage-editor-section__edit">
+        <h2>Prompts</h2>
+        <SortableItems
+          onChange={(items) => { onChange({ prompts: items }) }}
+          component={Prompt}
+          items={items}
+        />
+      </Edit>
+      <Guidance className="stage-editor-section__guidance">
+        This is guidance for prompts.
+      </Guidance>
+    </Section>
+  );
+}
+
+const mapStateToProps = (state, props) => ({
+  show: has(props, 'stage.nodeType'),
+});
+
+export { Prompts };
+
+export default compose(
+  connect(mapStateToProps),
+)(Prompts);
---
+++ src/components/StageEditor/sections/SortableItems.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { compose, withHandlers, defaultProps } from 'recompose';
+import PropTypes from 'prop-types';
+import { SortableContainer, arrayMove } from 'react-sortable-hoc';
+
+const itemHandlers = withHandlers({
+  onSortEnd: props => ({ oldIndex, newIndex }) => {
+    const reorderedContentItems = arrayMove(props.items, oldIndex, newIndex);
+
+    props.onChange(reorderedContentItems);
+  },
+  updateItem: props => (newItem, index) => {
+    const items = props.items
+      .map((item, i) => {
+        if (i !== index) { return item; }
+        return newItem;
+      });
+
+    props.onChange(items);
+  },
+  deleteItem: props => (index) => {
+    const items = props.items
+      .filter((_, i) => i !== index);
+
+    props.onChange(items);
+  },
+});
+
+const SortableItems = ({ items, updateItem, deleteItem, component: Component }) => (
+  <div className="stage-editor-sortable-items">
+    { items.map(
+      (props, index) => (
+        <Component
+          {...props}
+          index={index}
+          key={index}
+          onChange={item => updateItem(item, index)}
+          onDelete={() => deleteItem(index)}
+        />
+      ),
+    ) }
+  </div>
+);
+
+SortableItems.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+  updateItem: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  component: PropTypes.element.isRequired,
+};
+
+SortableItems.defaultProps = {
+  items: [],
+};
+
+export default compose(
+  defaultProps({ lockAxis: 'y', useDragHandle: true }),
+  itemHandlers,
+  SortableContainer,
+)(SortableItems);
