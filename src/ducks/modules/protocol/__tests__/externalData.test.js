73f6307d37b7a046e176ec69779b4fbbc8384cbb
Steve Mckellar <steve@wwqrd.uk>
2018-04-12 12:31:25 +0100

    Added noop reducer tests

---
+++ src/ducks/modules/protocol/__tests__/externalData.test.js
@@ -0,0 +1,16 @@
+/* eslint-env jest */
+
+import reducer from '../externalData';
+
+describe('protocol.externalData', () => {
+  it('does nothing', () => {
+    const noop = reducer(
+      [],
+      {},
+    );
+
+    expect(
+      noop,
+    ).toEqual([]);
+  });
+});
