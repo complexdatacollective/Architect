a491380b5766f6a4e17134c3a791d19cc00f1647
Steve Mckellar <steve@wwqrd.uk>
2018-04-25 13:40:31 +0100

    Sketch overview layout

--- src/components/Timeline/Overview.js
+++ src/components/Timeline/Overview.js
@@ -1,22 +1,35 @@
+/* eslint-disable */
+
 import React from 'react';
 import Tweened from '../../behaviours/Tweened';
 
-const Panel = Tweened(() => (
+const Panel = Tweened(({ children }) => (
   <div className="panel">
-    Hello wosoeidnfpgfp.foog
-    fpg<br />
-    Hello wosoeidnfpgfp.foog
-    fpg<br />
-    Hello wosoeidnfpgfp.foog
-    fpg<br />Hello wosoeidnfpgfp.foog
-    fpg<br />
+    { children }
   </div>
 ));
 
-const TimelineOverview = () => (
+const Overview = ({ title }) => (
   <div className="timeline-overview">
-    <Panel tweenName="protocol" tweenElement="overview-panel" />
+    <Panel tweenName="protocol" tweenElement="overview-panel">
+      { title }
+
+      <div className="panel__groups">
+        <div className="panel__group">
+          <h3 className="panel__group-title">Variable registry</h3>
+        </div>
+        <div className="panel__group">
+          <h3 className="panel__group-title">Forms</h3>
+        </div>
+        <div className="panel__group">
+          <h3 className="panel__group-title">Global Options</h3>
+        </div>
+        <div className="panel__group">
+          <h3 className="panel__group-title">Assets</h3>
+        </div>
+      </div>
+    </Panel>
   </div>
 );
 
-export default TimelineOverview;
+export default Overview;
