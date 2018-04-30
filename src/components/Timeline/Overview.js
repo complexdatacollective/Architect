597fe55f221a2ef36c698bcc549d82cc721f940c
Steve Mckellar <steve@wwqrd.uk>
2018-04-30 11:33:28 +0100

    Increase protocol overview height

--- src/components/Timeline/Overview.js
+++ src/components/Timeline/Overview.js
@@ -4,32 +4,30 @@ import React from 'react';
 import Tweened from '../../behaviours/Tweened';
 
 const Panel = Tweened(({ children }) => (
-  <div className="panel">
+  <div className="timeline-overview">
     { children }
   </div>
 ));
 
 const Overview = ({ title }) => (
-  <div className="timeline-overview">
-    <Panel tweenName="protocol" tweenElement="overview-panel">
-      { title }
+  <Panel tweenName="protocol" tweenElement="overview-panel">
+    <h2 className="timeline-overview__title">{ title }</h2>
 
-      <div className="panel__groups">
-        <div className="panel__group">
-          <h3 className="panel__group-title">Variable registry</h3>
-        </div>
-        <div className="panel__group">
-          <h3 className="panel__group-title">Forms</h3>
-        </div>
-        <div className="panel__group">
-          <h3 className="panel__group-title">Global Options</h3>
-        </div>
-        <div className="panel__group">
-          <h3 className="panel__group-title">Assets</h3>
-        </div>
+    <div className="panel__groups">
+      <div className="panel__group">
+        <h3 className="panel__group-title">Variable registry</h3>
       </div>
-    </Panel>
-  </div>
+      <div className="panel__group">
+        <h3 className="panel__group-title">Forms</h3>
+      </div>
+      <div className="panel__group">
+        <h3 className="panel__group-title">Global Options</h3>
+      </div>
+      <div className="panel__group">
+        <h3 className="panel__group-title">Assets</h3>
+      </div>
+    </div>
+  </Panel>
 );
 
 export default Overview;
