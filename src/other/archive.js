9dfe31771755e7ece16eb924b898055a55a587ac
Steve Mckellar <steve@wwqrd.uk>
2018-05-11 17:20:02 +0100

    Update archive to use `path`, not regexp

--- src/other/archive.js
+++ src/other/archive.js
@@ -8,28 +8,30 @@ const archiveOptions = {
   zlib: { level: 9 },
 };
 
-const getProtocolName = fileName => /.*\/([^/\\]+)\.netcanvas$/.exec(fileName)[1];
+const getProtocolName = fileName => path.basename(fileName, '.netcanvas');
 const getLocalDirectory = protocolName => path.join(remote.getPath('temp'), protocolName);
 
 // returns promise
-const extract = fileName =>
-  decompress(
-    fileName,
-    getLocalDirectory(getProtocolName(fileName)),
-  );
+const extract = (fileName) => {
+  const workingPath = getLocalDirectory(getProtocolName(fileName));
 
-const archive = (protocolName, fileName) =>
+  return decompress(
+    fileName,
+    workingPath,
+  ).then(() => workingPath);
+};
+
+const archive = (workingPath, fileName) =>
   new Promise((resolve, reject) => {
     const output = fs.createWriteStream(fileName);
     const zip = archiver('zip', archiveOptions);
-    const source = getLocalDirectory(protocolName);
 
     output.on('end', () => { resolve(fileName); });
 
     zip.on('warning', reject);
     zip.on('error', reject);
 
-    zip.directory(source, false);
+    zip.directory(workingPath, false);
 
     zip.finalize();
   });
