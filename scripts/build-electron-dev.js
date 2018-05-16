e714dc05a78ca4236acc193e9ddb085aef692010
Bryan Fox <git@bryfox.com>
2018-05-16 11:09:32 +0300

    Remove duplicate copy task

--- scripts/build-electron-dev.js
+++ scripts/build-electron-dev.js
@@ -1,14 +1,8 @@
 const fs = require('fs-extra');
 const paths = require('../config/paths');
 
-const mainSrc = paths.appPublic;
-
 function copySrc() {
   fs.emptyDirSync(paths.electronDev);
-
-  fs.copySync(mainSrc, paths.electronDev, {
-    filter: file => !(/__tests__/).test(file),
-  });
 
   fs.copySync(paths.appPublic, paths.electronDev, {
     dereference: true,
