514c8abe362c386c662eb12676904d1f0d612380
Steve Mckellar <steve@wwqrd.uk>
2018-05-17 09:56:00 +0100

    Update shims for non-electron testing

--- src/utils/webShims/electron.js
+++ src/utils/webShims/electron.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 
-const showSaveDialog = (_, callback) => { callback('demo.canvas'); };
-const showOpenDialog = (_, callback) => { callback('demo.canvas'); };
+const showSaveDialog = (_, callback) => { callback('demo.netcanvas'); };
+const showOpenDialog = (_, callback) => { callback('demo.netcanvas'); };
 
 module.exports = {
   remote: {
     dialog: {
       showSaveDialog,
       showOpenDialog,
+    },
+    app: {
+      getPath: () => '/tmp/foo/bar',
     },
   },
 };
