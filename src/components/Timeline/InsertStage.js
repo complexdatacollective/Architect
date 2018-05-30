d2b86c31d5a5d884f6d048dc198421530f00de8a
Steve Mckellar <steve@wwqrd.uk>
2018-05-30 11:04:35 +0100

    Titles for screen options

--- src/components/Timeline/InsertStage.js
+++ src/components/Timeline/InsertStage.js
@@ -44,6 +44,41 @@ const interfaceOptions = [
       description: (<p>{'The Information Interface allows you to display text and rich media (including pictures, video and audio) to your participants. Use it to introduce your research, help explain interview tasks, or illustrate concepts or ideas.'}</p>),
     },
   },
+  {
+    type: 'NameGenerator',
+    guidance: {
+      title: 'Name Generator',
+      description: (<p>{'The Name Generator interface is designed to prompt your research participants to name alters. It includes the ability to include external network data in the form of a roster.'}</p>),
+    },
+  },
+  {
+    type: 'Sociogram',
+    guidance: {
+      title: 'Sociogram',
+      description: (<div><p>{'The Sociogram interface allows your participants to position alters spatially, either using the concentric circles framework, or a background image of your choosing.'}</p><p>{'It also allows the creation of edges between alters based on any criteria, and the nomination of alters using an boolean variable.'}</p></div>),
+    },
+  },
+  {
+    type: 'Ordinal Bin',
+    guidance: {
+      title: 'Ordinal Bin',
+      description: (<p>{'The Ordinal Bin interface allows your participants to quickly assign the value of an ordinal variable to an alter or edge.'}</p>),
+    },
+  },
+  {
+    type: 'Categorize',
+    guidance: {
+      title: 'Categorical Bin',
+      description: (<p>{'The Categorical Bin interface allows your participants to quickly assign the value of a categorical variable to an alter or edge.'}</p>),
+    },
+  },
+  {
+    type: 'Information',
+    guidance: {
+      title: 'Information',
+      description: (<p>{'The Information Interface allows you to display text and rich media (including pictures, video and audio) to your participants. Use it to introduce your research, help explain interview tasks, or illustrate concepts or ideas.'}</p>),
+    },
+  },
 ];
 
 const StageType = Zoom(
@@ -58,8 +93,13 @@ const StageType = Zoom(
         onMouseEnter={onMouseEnterStageType}
         onMouseLeave={onMouseLeaveStageType}
       >
-        { image && <img className="timeline-insert-stage__option-preview" src={image} alt={type} /> }
-        { !image && <div className="timeline-insert-stage__option-description">{type} Interface</div> }
+        <h3>{ type }</h3>
+        <div
+          className="timeline-insert-stage__option-screen"
+        >
+          { image && <img className="timeline-insert-stage__option-preview" src={image} alt={type} /> }
+          { !image && <div className="timeline-insert-stage__option-description">{type} Interface</div> }
+        </div>
       </div>
     );
   },
@@ -129,8 +169,10 @@ class InsertStage extends PureComponent {
 
     return (
       <div className="timeline-insert-stage">
-        <div className="timeline-insert-stage__options">
-          {interfaceOptions.map(this.renderOption)}
+        <div className="timeline-insert-stage__chooser">
+          <div className="timeline-insert-stage__options">
+            {interfaceOptions.map(this.renderOption)}
+          </div>
         </div>
         <div className={guidanceClasses}>
           <div className="timeline-insert-stage__guidance-introduction">
