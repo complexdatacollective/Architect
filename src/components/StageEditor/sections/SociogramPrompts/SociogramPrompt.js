2d549f54a2bde7aa729ae86d8fb6eaa1f75571c0
Steve Mckellar <steve@wwqrd.uk>
2018-06-06 18:11:30 +0100

    WIP background

--- src/components/StageEditor/sections/SociogramPrompts/SociogramPrompt.js
+++ src/components/StageEditor/sections/SociogramPrompts/SociogramPrompt.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
@@ -37,64 +37,102 @@ import * as Fields from '../../../Form/Fields';
 //   }
 // }
 
-const NameGeneratorPrompt = ({
-  fieldId,
-  nodeTypes,
-}) => (
-  <div className="stage-editor-section-name-generator-prompt">
-    <div className="stage-editor-section-name-generator-prompt__setting">
-      <div className="stage-editor-section-name-generator-prompt__setting-label">Text for prompt</div>
-      <Field
-        name={`${fieldId}.text`}
-        component={Fields.Markdown}
-        className="stage-editor-section-name-generator-prompt__setting-value"
-        placeholder="Enter text for the prompt here"
-      />
-    </div>
-    <div className="stage-editor-section-name-generator-prompt__setting">
-      <div className="stage-editor-section-name-generator-prompt__setting-label">Which nodes?</div>
-      <Field
-        name={`${fieldId}.subject`}
-        parse={value => ({ type: value, entity: 'node' })}
-        format={value => get(value, 'type')}
-        options={nodeTypes}
-        component={Fields.Contexts}
-      />
-    </div>
-    <div className="stage-editor-section-name-generator-prompt__setting">
-      <div className="stage-editor-section-name-generator-prompt__setting-label">Layout</div>
-      <Field
-        name={`${fieldId}.layout.layoutVariable`}
-        component={Fields.Text}
-        className="stage-editor-section-name-generator-prompt__setting-value"
-        placeholder="Give us a variable name"
-      />
-      <Field
-        name={`${fieldId}.layout.allowPositioning`}
-        component={Fields.Checkbox}
-        className="stage-editor-section-name-generator-prompt__setting-value"
-      />
-    </div>
-  </div>
-);
+class SociogramPrompt extends Component {
 
-NameGeneratorPrompt.propTypes = {
-  fieldId: PropTypes.string.isRequired,
-  nodeTypes: PropTypes.array.isRequired,
-};
+  static propTypes = {
+    fieldId: PropTypes.string.isRequired,
+    nodeTypes: PropTypes.array.isRequired,
+  };
 
-NameGeneratorPrompt.defaultProps = {
-};
+  static defaultProps = {
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      backgroundType: 'circles',
+    };
+  }
+
+  render() {
+    const {
+      fieldId,
+      nodeTypes,
+    } = this.props;
+
+    return (
+      <div className="stage-editor-section-name-generator-prompt">
+        <div className="stage-editor-section-name-generator-prompt__setting">
+          <div className="stage-editor-section-name-generator-prompt__setting-label">Text for prompt</div>
+          <Field
+            name={`${fieldId}.text`}
+            component={Fields.Markdown}
+            className="stage-editor-section-name-generator-prompt__setting-value"
+            placeholder="Enter text for the prompt here"
+          />
+        </div>
+        <div className="stage-editor-section-name-generator-prompt__setting">
+          <div className="stage-editor-section-name-generator-prompt__setting-label">Which nodes?</div>
+          <Field
+            name={`${fieldId}.subject`}
+            parse={value => ({ type: value, entity: 'node' })}
+            format={value => get(value, 'type')}
+            options={nodeTypes}
+            component={Fields.Contexts}
+          />
+        </div>
+        <div className="stage-editor-section-name-generator-prompt__setting">
+          <div className="stage-editor-section-name-generator-prompt__setting-label">Layout</div>
+          <Field
+            name={`${fieldId}.layout.layoutVariable`}
+            component={Fields.Text}
+            label="Layout variable"
+            className="stage-editor-section-name-generator-prompt__setting-value"
+            placeholder="MY_VARIABLE"
+          />
+          <Field
+            name={`${fieldId}.layout.allowPositioning`}
+            component={Fields.Checkbox}
+            label="Allow positioning?"
+            className="stage-editor-section-name-generator-prompt__setting-value"
+          />
+        </div>
+        <div className="stage-editor-section-name-generator-prompt__setting">
+          <div className="stage-editor-section-name-generator-prompt__setting-label">Background</div>
+          <div>
+            <p>Background type:</p>
+            <Fields.Radio label="Circles" input={{ value: 'circles', name: 'backgroundType' }} />
+            <Fields.Radio label="Circles" input={{ value: 'image', name: 'backgroundType' }} />
+          </div>
+          <Field
+            name={`${fieldId}.background.concentricCircles`}
+            component={Fields.Text}
+            label="How many circles?"
+            className="stage-editor-section-name-generator-prompt__setting-value"
+            placeholder="5"
+          />
+          <Field
+            name={`${fieldId}.background.skewedTowardCenter`}
+            component={Fields.Checkbox}
+            label="Skewed towards center?"
+            className="stage-editor-section-name-generator-prompt__setting-value"
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   nodeTypes: keys(state.protocol.present.variableRegistry.node),
   edgeTypes: keys(state.protocol.present.variableRegistry.edge),
 });
 
-export { NameGeneratorPrompt };
+export { SociogramPrompt };
 
 export default connect(
   mapStateToProps,
 )(
-  NameGeneratorPrompt,
+  SociogramPrompt,
 );
