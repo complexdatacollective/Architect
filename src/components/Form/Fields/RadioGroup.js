4396ae518ccaf55765ecc06e6d593ca6a513d98e
Steve Mckellar <steve@wwqrd.uk>
2018-06-12 16:38:28 +0100

    Update RadioGroup to use <Radio /> internally

--- src/components/Form/Fields/RadioGroup.js
+++ src/components/Form/Fields/RadioGroup.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { get, isString } from 'lodash';
 import cx from 'classnames';
 import uuid from 'uuid';
+import Radio from './Radio';
 
 const toString = value => (isString(value) ? value : JSON.stringify(value));
 const getValue = option => get(option, 'value', option);
@@ -29,32 +30,21 @@ class RadioGroup extends Component {
   renderOption = (option, index) => {
     const {
       input: { value },
-      name,
     } = this.props;
+
     const optionValue = getValue(option);
     const optionLabel = getLabel(option);
     const selected = optionValue === value;
-    const optionClasses = cx(
-      'form-fields-radio-group__option',
-      { 'form-fields-radio-group__option--selected': selected },
-    );
 
     return (
-      <label className={optionClasses} htmlFor={`${this.id}_${index}`}>
-        <input
-          id={`${this.id}_${index}`}
-          className="form-fields-radio-group__option-input"
-          type="radio"
-          name={name}
-          value={index}
-          checked={selected}
-          onChange={this.onChange}
-        />
-        <div className="form-fields-radio-group__option-radio" />
-        <div className="form-fields-radio-group__option-label">
-          {optionLabel}
-        </div>
-      </label>
+      <Radio
+        input={{
+          value: index,
+          checked: selected,
+          onChange: this.onChange,
+        }}
+        label={optionLabel}
+      />
     );
   }
 
