4cfaf2477ed6c48e5dcee6cca0e3660beb91c0c7
Steve Mckellar <steve@wwqrd.uk>
2018-06-18 14:47:27 +0100

    Set up locales

---
+++ src/locales/en_US.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+export default {
+  'guidance.title': (
+    <Fragment>
+      <p>
+        A good stage title should be descriptive, but not too long. It should help you to remember
+        the purpose of this stage later.
+      </p>
+      <p>
+        It might help to use a standard format for the names of your interview stages, such as
+        <code>[TYPE]: [VARIABLE]</code>
+      </p>
+      <p>
+        This text is displayed in the menu within Network Canvas, and on the timeline in Architect.
+      </p>
+    </Fragment>
+  ),
+};
