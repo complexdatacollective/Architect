84f9bdcdcffb247a57bff16b19f2b369485a09e4
Steve Mckellar <steve@wwqrd.uk>
2018-06-20 13:32:39 +0100

    Move interface guidance into locales

--- src/components/StageEditor/Interfaces/Information.js
+++ src/components/StageEditor/Interfaces/Information.js
@@ -5,16 +5,6 @@ import {
 } from '../sections';
 
 const Information = {
-  guidance: (
-    <React.Fragment>
-      <h3>Information screen guidance</h3>
-      <p>
-        The Information Interface allows you to display text and rich media (including pictures,
-        video and audio) to your participants. Use it to help explain interview tasks, or introduce
-        concepts or ideas.
-      </p>
-    </React.Fragment>
-  ),
   sections: [
     Title,
     ContentItems,
--- src/locales/en_US.js
+++ src/locales/en_US.js
@@ -26,4 +26,30 @@ export default {
       </p>
     </Fragment>
   ),
+  'guidance.interface.NameGenerator': (
+    <Fragment>
+      <h3>Name Generator guidance</h3>
+      <p>
+        The Name Generator interface is designed to allow your research participants
+        to name alters for later analysis.
+      </p>
+      <p>
+        After giving your stage a descriptive title, you should determine the type
+        of node that you wish to elicit. Either choose from your existing node types,
+        or create a new one.
+      </p>
+      <p>
+        For further help with configuring the Name Generator interface, please refer
+        to our <a href={null}>Online Documentation</a>.
+      </p>
+    </Fragment>
+  ),
+  'guidance.interface.Sociogram': (
+    <Fragment>
+      <h3>Sociogram guidance</h3>
+      <p>
+        TBD.
+      </p>
+    </Fragment>
+  ),
 };
