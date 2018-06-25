1bf199e5debaa417a6dba71011561bce9a6eca7d
Steve Mckellar <steve@wwqrd.uk>
2018-06-25 16:56:00 +0100

    Fix key warnings

--- src/components/StageEditor/StageEditor.js
+++ src/components/StageEditor/StageEditor.js
@@ -20,7 +20,7 @@ class StageEditor extends Component {
   }
 
   renderSections() {
-    return this.sections.map(SectionComponent => <SectionComponent form={form} />);
+    return this.sections.map((SectionComponent, index) => <SectionComponent key={index} form={form} />);
   }
 
   render() {
