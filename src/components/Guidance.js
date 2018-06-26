d8a4985a5c7e80319d821e01b3200612581b4e4e
Steve Mckellar <steve@wwqrd.uk>
2018-06-26 14:51:02 +0100

    'Focus' guidance prototype

--- src/components/Guidance.js
+++ src/components/Guidance.js
@@ -4,13 +4,41 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators as guidedActionCreators } from '../ducks/modules/guidance';
 
+const MOUSE = 'MOUSE';
+const FOCUS = 'FOCUS';
+
 class Guided extends Component {
   static propTypes = {
     contentId: PropTypes.string.isRequired,
     setGuidance: PropTypes.func.isRequired,
     clearGuidance: PropTypes.func.isRequired,
     children: PropTypes.node.isRequired,
+    interaction: PropTypes.oneOf([
+      MOUSE,
+      FOCUS,
+    ]),
   };
+
+  static defaultProps = {
+    interaction: MOUSE,
+  };
+
+  get interactionHandlers() {
+    switch (this.props.interaction) {
+      case MOUSE:
+        return {
+          onMouseEnter: this.handleMouseEnter,
+          onMouseLeave: this.handleMouseLeave,
+        };
+      case FOCUS:
+        return {
+          onFocus: this.handleFocus,
+          onBlur: this.handleBlur,
+        };
+      default:
+        return {};
+    }
+  }
 
   handleMouseEnter = () => {
     this.props.setGuidance(this.props.contentId);
@@ -20,13 +48,18 @@ class Guided extends Component {
     this.props.clearGuidance();
   }
 
+  handleFocus = () => {
+    this.props.setGuidance(this.props.contentId);
+  }
+
+  handleBlur = () => {
+    this.props.clearGuidance();
+  }
+
   render() {
     return React.cloneElement(
       this.props.children,
-      {
-        onMouseEnter: this.handleMouseEnter,
-        onMouseLeave: this.handleMouseLeave,
-      },
+      this.interactionHandlers,
     );
   }
 }
--- src/ducks/modules/guidance.js
+++ src/ducks/modules/guidance.js
@@ -3,28 +3,34 @@ const RESET_GUIDANCE = Symbol('PROTOCOL/RESET_GUIDANCE');
 
 const initialState = {
   id: null,
+  locked: false,
 };
 
-const setGuidance = id =>
+const setGuidance = (id, lock = false) =>
   ({
     type: SET_GUIDANCE,
     id,
+    lock,
   });
 
-const resetGuidance = () =>
+const resetGuidance = (unlock = false) =>
   ({
     type: RESET_GUIDANCE,
+    unlock,
   });
 
 
 function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case SET_GUIDANCE:
+      if (state.locked && !action.lock) { return state; }
       return {
         ...state,
         id: action.id,
+        locked: action.lock,
       };
     case RESET_GUIDANCE:
+      if (state.locked && !action.unlock) { return state; }
       return { ...initialState };
     default:
       return state;
