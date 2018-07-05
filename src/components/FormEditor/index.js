44b054a981ed094ab407107fe33ace56d981acc1
Steve Mckellar <steve@wwqrd.uk>
2018-07-05 12:49:07 +0100

    Linted FormEditor

--- src/components/FormEditor/index.js
+++ src/components/FormEditor/index.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
-
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import {
   reduxForm,
   formValueSelector,
+  change,
+  isDirty,
+  isValid,
 } from 'redux-form';
 import { compose, withState, withHandlers } from 'recompose';
 import { keys } from 'lodash';
@@ -14,19 +14,30 @@ import FormEditor from './FormEditor';
 
 const formName = 'edit-form';
 const getFormValue = formValueSelector(formName);
+const getIsDirty = isDirty(formName);
+const getIsValid = isValid(formName);
 
 const mapStateToProps = (state) => {
   const formNodeType = getFormValue(state, 'type') || null;
 
   return {
-    getFormValue,
+    nodeType: formNodeType,
     nodeTypes: keys(getNodeTypes(state)),
     variables: getVariablesForNodeType(state, formNodeType),
+    dirty: getIsDirty(state),
+    valid: getIsValid(state),
   };
 };
 
+const mapDispatchToProps = dispatch => ({
+  resetFields: () => {
+    dispatch(change(formName, 'type', null));
+    dispatch(change(formName, 'fields', null));
+  },
+});
+
 export default compose(
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
   withState('codeView', 'updateCodeView', false),
   withHandlers({
     toggleCodeView: ({ updateCodeView }) => () => updateCodeView(current => !current),
