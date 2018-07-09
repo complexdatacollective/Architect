724d37a0bfc43b2dbdc3d95d477711f6814b540f
Steve Mckellar <steve@wwqrd.uk>
2018-07-09 17:37:26 +0100

    Update ValidatedField code style based on team preferences

--- src/components/Form/ValidatedField.js
+++ src/components/Form/ValidatedField.js
@@ -1,16 +1,29 @@
-import { lifecycle } from 'recompose';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
 import { getValidations } from '../../utils/validations';
 
 /*
- * This converts our `validation` object into redux-form compatible `validate` methods.
- * Using `componentWillMount()` is a requirement as redux-form will re-register a field component
- * if validate appears to change, and since we use function generators, identites will not match.
+ * This converts our `validation` object into redux-form compatible `validate` prop.
+ * Using `constructor()` is a requirement as redux-form will re-register a field component
+ * if validate appears to change, and since we use function generators, identites will not match
+ * if we fetch this directly in the render method.
  */
-const ValidatedField = lifecycle({
-  componentWillMount() {
-    this.setState({ validate: getValidations(this.props.validation || []) });
-  }
-})(Field);
+const withValidation = WrappedComponent =>
+  class Validated extends Component {
+    static propTypes = {
+      validation: PropTypes.object.isRequired,
+    };
 
-export default ValidatedField;
+    constructor(props) {
+      super(props);
+
+      this.validate = getValidations(props.validation || []);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} validate={this.validate} />;
+    }
+  };
+
+export default withValidation(Field);
