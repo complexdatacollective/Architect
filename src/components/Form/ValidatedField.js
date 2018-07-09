27dd75cee0d5cd2703af48d6a67fcaafb1309dc6
Steve Mckellar <steve@wwqrd.uk>
2018-07-09 16:59:04 +0100

    Fix ValidatedField

--- src/components/Form/ValidatedField.js
+++ src/components/Form/ValidatedField.js
@@ -1,6 +1,16 @@
+import { lifecycle } from 'recompose';
 import { Field } from 'redux-form';
-import withValidation from '../../behaviours/withValidation';
+import { getValidations } from '../../utils/validations';
 
-const ValidatedField = withValidation(Field);
+/*
+ * This converts our `validation` object into redux-form compatible `validate` methods.
+ * Using `componentWillMount()` is a requirement as redux-form will re-register a field component
+ * if validate appears to change, and since we use function generators, identites will not match.
+ */
+const ValidatedField = lifecycle({
+  componentWillMount() {
+    this.setState({ validate: getValidations(this.props.validation || []) });
+  }
+})(Field);
 
 export default ValidatedField;
