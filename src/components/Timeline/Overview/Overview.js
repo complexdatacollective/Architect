47ff8faf115cef1a7d882efc73d4474bb6c363d1
Steve Mckellar <steve@wwqrd.uk>
2018-07-12 15:03:46 +0100

    Remove references to form editor

--- src/components/Timeline/Overview/Overview.js
+++ src/components/Timeline/Overview/Overview.js
@@ -2,50 +2,14 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { get, keys } from 'lodash';
-import { toPairs } from 'lodash/fp';
 import { compose } from 'recompose';
 import { SeamlessText } from '../../Form/Fields';
 import { Tweened } from '../../../behaviours/Tweened';
 import { Node, Icon } from '../../../ui';
 import { actionCreators as protocolActions } from '../../../ducks/modules/protocol';
 import PanelGroup from './PanelGroup';
-import Zoom from '../../../behaviours/Zoom';
-import { getCSSVariableAsString } from '../../../utils/CSSVariables';
-
-const zoomColors = [getCSSVariableAsString('--color-sea-green'), '#ffffff'];
-
-const EditFormButton = Zoom(
-  ({ formPath }) => <Link to={formPath} className="button button--small">edit</Link>,
-);
-
-const NewFormButton = Zoom(
-  ({ protocol }) => (
-    <Link
-      to={`/edit/${encodeURIComponent(protocol)}/form/`}
-      className="button button--small"
-    >
-      New form
-    </Link>
-  ),
-);
-
-const renderForm = (formName, protocol) => {
-  const formPath = `/edit/${encodeURIComponent(protocol)}/form/${formName}`;
-
-  return (
-    <div className="timeline-overview__form" key={formName}>
-      <div className="timeline-overview__form-name">
-        {formName}
-      </div>
-      <div className="timeline-overview__form-edit">
-        <EditFormButton formPath={formPath} zoomColors={zoomColors} />
-      </div>
-    </div>
-  );
-};
 
 class Overview extends Component {
   get renderNodeTypes() {
@@ -91,22 +55,6 @@ class Overview extends Component {
     );
   }
 
-  get renderForms() {
-    const forms = toPairs(this.props.forms);
-    return (
-      <div>
-        <div className="timeline-overview__forms">
-          {forms.map(
-            ([formName]) => renderForm(formName, this.props.match.params.protocol),
-          )}
-        </div>
-        <div className="timeline-overview__new-form">
-          <NewFormButton protocol={this.props.match.params.protocol} zoomColors={zoomColors} />
-        </div>
-      </div>
-    );
-  }
-
   render() {
     const {
       name,
@@ -136,7 +84,7 @@ class Overview extends Component {
                   { this.renderEdgeTypes }
                 </PanelGroup>
                 <PanelGroup title="Forms">
-                  { this.renderForms }
+                  <p>Forms not displayed yet.</p>
                 </PanelGroup>
                 <PanelGroup title="Global Options">
                   <p>Version: {version}</p>
@@ -160,7 +108,6 @@ Overview.propTypes = {
   variableRegistry: PropTypes.object,
   externalData: PropTypes.object, // eslint-disable-line react/no-unused-prop-types
   updateOptions: PropTypes.func,
-  match: PropTypes.object.isRequired,
 };
 
 Overview.defaultProps = {
