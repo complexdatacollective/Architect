dd618042e3c9437a583ea358e5000645d329522f
Steve Mckellar <steve@wwqrd.uk>
2018-07-17 18:40:54 +0100

    Update EditType to parse/format variables

--- src/components/Cards/EditType.js
+++ src/components/Cards/EditType.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { submit, isDirty, isInvalid } from 'redux-form';
-import { get } from 'lodash';
+import { get, mapValues, values, reduce } from 'lodash';
 import { Button } from '../../ui/components';
 import TypeEditor, { formName } from '../TypeEditor';
 import Card from './ProtocolCard';
 import { getProtocol } from '../../selectors/protocol';
-import { actionCreators as variableRegistryActions} from '../../ducks/modules/protocol/variableRegistry';
+import { actionCreators as variableRegistryActions } from '../../ducks/modules/protocol/variableRegistry';
 
 /**
  * This manages the display of the TypeEditor, provides it with
@@ -16,7 +16,11 @@ import { actionCreators as variableRegistryActions} from '../../ducks/modules/pr
  */
 class EditType extends PureComponent {
   static propTypes = {
+    initialValues: PropTypes.object.isRequired,
     submit: PropTypes.func.isRequired,
+    updateType: PropTypes.func.isRequired,
+    category: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
     hasUnsavedChanges: PropTypes.bool,
     hasErrors: PropTypes.bool,
     onComplete: PropTypes.func.isRequired,
@@ -34,7 +38,6 @@ class EditType extends PureComponent {
       category,
       type,
     } = this.props;
-    console.log('OnSubmit', form);
 
     this.props.updateType(category, type, form);
 
@@ -84,17 +87,38 @@ class EditType extends PureComponent {
   }
 }
 
+const parse = configuration => ({
+  ...configuration,
+  variables: values(mapValues(
+    get(configuration, 'variables', {}),
+    (variable, key) => ({
+      name: key,
+      ...variable,
+    }),
+  )),
+});
+
+const format = configuration => ({
+  ...configuration,
+  variables: reduce(
+    configuration.variables,
+    (memo, { name, ...variable }) => ({
+      ...memo,
+      [name]: variable,
+    }),
+    {},
+  ),
+});
+
 const editFormIsDirty = isDirty(formName);
 const editFormIsInvalid = isInvalid(formName);
 
 function mapStateToProps(state, props) {
-  const protocol = getProtocol(state);
-  const initialValues = get(protocol, 'variableRegistry.node.person');
-
-  console.log(props);
-
   const category = get(props, 'match.params.category');
   const type = get(props, 'match.params.type');
+
+  const protocol = getProtocol(state);
+  const initialValues = parse(get(protocol, ['variableRegistry', category, type]));
 
   return {
     initialValues,
@@ -107,7 +131,8 @@ function mapStateToProps(state, props) {
 
 const mapDispatchToProps = dispatch => ({
   submit: bindActionCreators(submit, dispatch),
-  updateType: bindActionCreators(variableRegistryActions.updateType, dispatch),
+  updateType: (category, type, form) =>
+    dispatch(variableRegistryActions.updateType(category, type, format(form))),
 });
 
 export { EditType };
