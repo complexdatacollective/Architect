f5aa92e639559e51049eb850aee15261727ff6e0
Steve Mckellar <steve@wwqrd.uk>
2018-07-17 13:03:01 +0100

    Fix for missing assets in built app
    Resolves #151

--- src/history.js
+++ src/history.js
@@ -1,6 +1,18 @@
 
 import createHistory from 'history/createBrowserHistory';
+import createMemoryHistory from 'history/createMemoryHistory';
+import process from 'process';
 
-const history = createHistory();
+const getHistory = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return createHistory();
+  }
+
+  // Packaged app confuses assets for page routes. This fixes it. Ulimately a better
+  // configured webpack would probably be the best solution
+  return createMemoryHistory();
+};
+
+const history = getHistory();
 
 export default history;
