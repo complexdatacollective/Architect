f713f37ff63266d750fd9906454b34be8cb317cc
Steve Mckellar <steve@wwqrd.uk>
2018-07-19 15:17:11 +0100

    Added updateType test to variableRegistry

--- src/ducks/modules/protocol/__tests__/variableRegistry.test.js
+++ src/ducks/modules/protocol/__tests__/variableRegistry.test.js
@@ -1,16 +1,34 @@
 /* eslint-env jest */
 
-import reducer from '../variableRegistry';
+import reducer, { actionCreators } from '../variableRegistry';
+
+const mockState = {
+  node: { place: { foo: 'bar' } },
+  edge: { },
+};
 
 describe('protocol.variableRegistry', () => {
   it('does nothing', () => {
-    const noop = reducer(
-      [],
-      {},
+    const noop = reducer();
+    expect(noop).toEqual({});
+  });
+
+  it('updateType()', () => {
+    const nextState = reducer(
+      mockState,
+      actionCreators.updateType(
+        'node',
+        'person',
+        { bazz: 'buzz' },
+      ),
     );
 
-    expect(
-      noop,
-    ).toEqual([]);
+    expect(nextState).toEqual({
+      node: {
+        place: { foo: 'bar' },
+        person: { bazz: 'buzz' },
+      },
+      edge: { },
+    });
   });
 });
