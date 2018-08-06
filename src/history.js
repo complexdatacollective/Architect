1a237fdf4a2703e5f5b6274aafb12cdd9f36f74e
Steve Mckellar <steve@wwqrd.uk>
2018-08-06 12:20:19 +0100

    Fix: Use production settings by default in history.js

--- src/history.js
+++ src/history.js
@@ -1,15 +1,17 @@
 
-import createHistory from 'history/createBrowserHistory';
+import createBrowserHistory from 'history/createBrowserHistory';
 import createMemoryHistory from 'history/createMemoryHistory';
 import process from 'process';
 
 const getHistory = () => {
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    return createHistory();
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    console.log('browser history', process.env.NODE_ENV);
+    return createBrowserHistory();
   }
 
   // Packaged app confuses assets for page routes. This fixes it. Ulimately a better
   // configured webpack would probably be the best solution
+  console.log('memory history', process.env.NODE_ENV);
   return createMemoryHistory();
 };
 
