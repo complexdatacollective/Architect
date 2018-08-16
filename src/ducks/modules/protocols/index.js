1633f592ea4d98a52e155e415699ba149827b6bd
Steve Mckellar <steve@wwqrd.uk>
2018-08-16 09:59:18 +0100

    Save and export thunk

--- src/ducks/modules/protocols/index.js
+++ src/ducks/modules/protocols/index.js
@@ -1,5 +1,33 @@
 import { importProtocol } from './import';
 import { createProtocol } from './create';
+import { saveProtocol } from './save';
+import { exportProtocol } from './export';
+
+const SAVE_AND_EXPORT = 'PROTOCOLS/SAVE_AND_EXPORT';
+const SAVE_AND_EXPORT_SUCCESS = 'PROTOCOLS/SAVE_AND_EXPORT_SUCCESS';
+const SAVE_AND_EXPORT_ERROR = 'PROTOCOLS/SAVE_AND_EXPORT_ERROR';
+
+const saveAndExportProtocol = () => ({
+  type: SAVE_AND_EXPORT,
+});
+
+const saveAndExportProtocolSuccess = () => ({
+  type: SAVE_AND_EXPORT_SUCCESS,
+});
+
+const saveAndExportProtocolError = error => ({
+  type: SAVE_AND_EXPORT_ERROR,
+  error,
+});
+
+const saveAndExportProtocolThunk = () =>
+  (dispatch) => {
+    dispatch(saveAndExportProtocol())
+      .then(() => dispatch(saveProtocol()))
+      .then(() => dispatch(exportProtocol()))
+      .then(() => dispatch(saveAndExportProtocolSuccess()))
+      .catch(e => dispatch(saveAndExportProtocolError(e)));
+  };
 
 const initialState = [];
 
@@ -13,6 +41,7 @@ export default function reducer(state = initialState, action = {}) {
 const actionCreators = {
   importProtocol,
   createProtocol,
+  saveAndExportProtocol: saveAndExportProtocolThunk,
 };
 
 const actionTypes = {
