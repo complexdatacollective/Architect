62c73af8f55a2cb8e9434567ea144b3a6c4a187d
Steve Mckellar <steve@wwqrd.uk>
2018-08-17 16:48:55 +0100

    get prototype working with new file loading changes

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -2,14 +2,11 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import { Flipper, Flipped } from 'react-flip-toolkit';
 import cx from 'classnames';
-import { actionCreators as protocolsActions } from '../ducks/modules/protocols';
 import ProtocolStack from './Views/Start/ProtocolStack';
-
 
 class Scene extends Component {
   static propTypes = {
@@ -56,6 +53,21 @@ class Scene extends Component {
     return this.state.mode === 'protocol';
   }
 
+  renderRecentProtocol = (protocol) => {
+    return (
+      <Flipped flipId={protocol.filePath} key={protocol.filePath}>
+        <div
+          className="scene__stack"
+          onClick={() => this.openStack(protocol.filePath)}
+        >
+          <ProtocolStack
+            protocol={protocol}
+          />
+        </div>
+      </Flipped>
+    );
+  }
+
   render() {
     const sceneClasses = cx(
       'scene',
@@ -68,19 +80,7 @@ class Scene extends Component {
       <div className={sceneClasses}>
         <Flipper flipKey={`${this.state.openStack}-${this.isProtocol()}`}>
           <div className="scene__stacks">
-            { !this.isProtocol() && this.props.protocols.map((protocol, index) => (
-              <Flipped flipId={protocol.id}>
-                <div
-                  className="scene__stack"
-                  key={protocol.id}
-                  onClick={() => this.openStack(protocol.id)}
-                >
-                  <ProtocolStack
-                    protocol={protocol}
-                  />
-                </div>
-              </Flipped>
-            )) }
+            { !this.isProtocol() && this.props.recentProtocols.map(this.renderRecentProtocol) }
           </div>
           <div className="scene__loader">
             <div onClick={this.props.createProtocol}>Create new</div>
@@ -107,12 +107,12 @@ class Scene extends Component {
 }
 
 const mapStateToProps = state => ({
-  // protocols: get(state, 'protocols', []).slice(0, 3),
+  recentProtocols: [{ filePath: 'rstst' }, { filePath: 'bar' }], //get(state, 'recentProtocols', []).slice(0, 3),
 });
 
 const mapDispatchToProps = dispatch => ({
-  createProtocol: bindActionCreators(protocolsActions.createProtocol, dispatch),
-  chooseProtocol: bindActionCreators(protocolsActions.chooseProtocol, dispatch),
+  // createProtocol: bindActionCreators(protocolsActions.createAndLoadProtocol, dispatch),
+  // chooseProtocol: bindActionCreators(protocolsActions.chooseProtocol, dispatch),
 });
 
 export { Scene };
