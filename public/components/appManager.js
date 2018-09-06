610044f34e44af3eb9b5aaab02ac41d0cc252379
Steve Mckellar <steve@wwqrd.uk>
2018-09-06 17:45:43 +0100

    Load devtools from NC_DEVTOOLS_EXTENSION_PATH

--- public/components/appManager.js
+++ public/components/appManager.js
@@ -1,4 +1,4 @@
-const { ipcMain, app } = require('electron');
+const { ipcMain, app, BrowserWindow } = require('electron');
 const path = require('path');
 const windowManager = require('./windowManager');
 const registerProtocolProtocol = require('./protocolProtocol').registerProtocolProtocol;
@@ -30,6 +30,23 @@ const appManager = {
         this.openFileWhenReady = null;
       }
     });
+  },
+  loadDevTools: () => {
+    const extensions = process.env.NC_DEVTOOLS_EXENSION_PATH;
+    if (process.env.NODE_ENV !== 'development' || !extensions) { return; }
+    try {
+      console.log(extensions);
+      extensions.split(';').forEach(
+        filepath =>
+          BrowserWindow.addDevToolsExtension(filepath),
+      );
+    } catch (err) {
+      /* eslint-disable no-console */
+      console.warn(err);
+      console.warn('A Chrome dev tools extension failed to load. If the extension has upgraded, update your NC_DEVTOOLS_EXENSION_PATH:');
+      console.warn(process.env.NC_DEVTOOLS_EXENSION_PATH);
+      /* eslint-enable */
+    }
   },
   openFileFromArgs: function openFileFromArgs(argv) {
     return this.restore()
