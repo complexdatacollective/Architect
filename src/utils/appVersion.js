f20b39464ca3b994b7ee9fa8200a34e1eb12e221
Steve Mckellar <steve@wwqrd.uk>
2018-09-09 14:36:49 +0100

    Updated version number to 4.0.0-alpha.4

--- src/utils/appVersion.js
+++ src/utils/appVersion.js
@@ -4,6 +4,7 @@ import { get } from 'lodash';
 const codenames = {
   '4.0.0-alpha.2': 'Anning',
   '4.0.0-alpha.3': 'Hercules',
+  '4.0.0-alpha.4': 'Gold-Bug',
 };
 
 const appVersion = remote.app.getVersion();
