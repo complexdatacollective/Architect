6a223019d0e2fe3167ed77c1e35c0cca55c4acb7
Steve Mckellar <steve@wwqrd.uk>
2018-09-10 16:42:44 +0100

    VariableEditor -> ItemEditor

--- src/components/StageEditor/sections/ContentGrid/ItemEditor.js
+++ src/components/StageEditor/sections/ContentGrid/ItemEditor.js
@@ -2,42 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Field } from 'redux-form';
-import * as fields from '../../../../ui/components/Fields';
 
-const VARIABLE_INPUT_TYPES = {
-  text: 'Text',
-  number: 'Text', // TODO: numeric only!
-  datetime: 'Text',
-  boolean: 'Toggle',
-  ordinal: 'RadioGroup',
-  categorical: 'CheckboxGroup',
-};
+const getInput = (name, item) =>
+  <Field name={name} />;
 
-const getInput = (name, variableMeta) => {
-  if (!name) { return null; }
-
-  const inputType = VARIABLE_INPUT_TYPES[variableMeta.type] || VARIABLE_INPUT_TYPES.text;
-  const inputComponent = fields[inputType];
-  const options = variableMeta.options || null;
-
-  return (
-    <Field
-      name={name}
-      label={variableMeta.label}
-      component={inputComponent}
-      options={options}
-    />
-  );
-};
-
-const VariableEditor = ({ name, variableMeta, show }) => (
-  <div className={cx('attributes-table-editor', { 'attributes-table-editor--show': show })}>
-    {getInput(name, variableMeta)}
+const VariableEditor = ({ name, item, show }) => (
+  <div className={cx('content-grid-editor', { 'content-grid-editor--show': show })}>
+    {getInput(name, item)}
   </div>
 );
 
 VariableEditor.propTypes = {
-  variableMeta: PropTypes.object.isRequired,
+  item: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   show: PropTypes.bool,
 };
