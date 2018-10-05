53dc6bf5db10b362a6bb4ac78abe582e3ab6fae3
Steve Mckellar <steve@wwqrd.uk>
2018-10-05 11:44:57 +0100

    Lint and update snapshots

--- src/components/ExampleForm.js
+++ src/components/ExampleForm.js
@@ -2,6 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../ui/components/Modal';
 
+/*
+ *  <ExampleForm
+ *    show={formOpen}
+ *    onComplete={() => {
+ *      // Form specific callback, probably also changes state of formOpen
+ *      closeForm();
+ *    }}
+ *  />
+ */
+
 const ExampleForm = ({ show, onComplete, onBlur }) => (
   <Modal show={show} onBlur={onBlur}>
     <div style={{ background: 'white', padding: '20px', borderRadius: '20px' }}>
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -4,7 +4,7 @@ import cx from 'classnames';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { Flipper } from 'react-flip-toolkit';
-import { compose, withStateHandlers } from 'recompose';
+import { compose } from 'recompose';
 import { getActiveProtocolMeta } from '../selectors/protocol';
 import Start from './Start';
 import RecentProtocols from './RecentProtocols';
@@ -12,18 +12,12 @@ import Overview from './Overview';
 import Timeline from './Timeline';
 import ProtocolControlBar from './ProtocolControlBar';
 import Cards from './Cards';
-import ExampleForm from './ExampleForm';
 import architectLogoIcon from '../images/architect-logo-icon.svg';
 import networkCanvasBrand from '../images/network-canvas-brand.svg';
-import { actionCreators as dialogsActions } from '../ducks/modules/dialogs';
 
 const Scene = ({
   protocolMeta,
   location,
-  formOpen,
-  openForm,
-  closeForm,
-  openDialog,
 }) => {
   const protocolId = protocolMeta && protocolMeta.id;
   const flipKey = protocolId || 'start';
@@ -40,27 +34,6 @@ const Scene = ({
       <div className="scene__background scene__background--top" />
       <div className="scene__background scene__background--bottom" />
       <img className="scene__brand" src={networkCanvasBrand} alt="" />
-      <div>
-        <button onClick={() => openDialog({ type: 'Notice' })}>Open notice</button>
-        <button onClick={() => openDialog({ type: 'Warning' })}>Open warning</button>
-        <button onClick={() => openDialog({ type: 'Confirm' })}>Open confirm</button>
-        <button
-          onClick={() => {
-            openDialog({ type: 'Confirm' });
-            openDialog({ type: 'Warning' });
-            openDialog({ type: 'Notice' });
-          }}
-        >Open lots of dialogs</button>
-        <button onClick={() => openForm()}>Open form</button>
-      </div>
-
-      <ExampleForm
-        show={formOpen}
-        onComplete={() => {
-          // Form specific callback, probably also changes state of formOpen
-          closeForm();
-        }}
-      />
 
       <NavLink className="scene__home" to="/" exact>
         <img src={architectLogoIcon} alt="" />
@@ -108,29 +81,8 @@ const mapStateToProps = state => ({
   protocolMeta: getActiveProtocolMeta(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  openDialog: dialog => dispatch(dialogsActions.openDialog(dialog)),
-});
-
-const testStateHandlers = withStateHandlers(
-  {
-    formOpen: false,
-  },
-  {
-    openForm: () =>
-      () => ({
-        formOpen: true,
-      }),
-    closeForm: () =>
-      () => ({
-        formOpen: false,
-      }),
-  },
-);
-
 export { Scene };
 
 export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  testStateHandlers,
+  connect(mapStateToProps),
 )(Scene);
