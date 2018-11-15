b38158dd26c4e9065616db472c89720051b5cfab
Steve Mckellar <steve@wwqrd.uk>
2018-11-15 12:34:51 +0000

    Confirm exit of protocol editor

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 import { Flipper } from 'react-flip-toolkit';
-import { compose } from 'recompose';
+import { compose, withHandlers } from 'recompose';
 import { getActiveProtocolMeta } from '../selectors/protocol';
+import { actionCreators as dialogActions } from '../ducks/modules/dialogs';
+import history from '../history';
 import Start from './Start';
 import RecentProtocols from './RecentProtocols';
 import Overview from './Overview';
@@ -18,6 +20,7 @@ import networkCanvasBrand from '../images/network-canvas-brand.svg';
 const Scene = ({
   protocolMeta,
   location,
+  handleClickStart,
 }) => {
   const protocolId = protocolMeta && protocolMeta.id;
   const flipKey = protocolId || 'start';
@@ -35,9 +38,9 @@ const Scene = ({
       <div className="scene__background scene__background--bottom" />
       <img className="scene__brand" src={networkCanvasBrand} alt="" />
 
-      <NavLink className="scene__home" to="/" exact>
+      <div onClick={handleClickStart} className="scene__home">
         <img src={architectLogoIcon} alt="" />
-      </NavLink>
+      </div>
 
       <Flipper flipKey={flipKey}>
 
@@ -71,6 +74,7 @@ const Scene = ({
 Scene.propTypes = {
   protocolMeta: PropTypes.object,
   location: PropTypes.object.isRequired,
+  handleClickStart: PropTypes.func.isRequired,
 };
 
 Scene.defaultProps = {
@@ -78,11 +82,36 @@ Scene.defaultProps = {
 };
 
 const mapStateToProps = state => ({
+  hasUnsavedChanges: (state.session.lastChanged > state.session.lastSaved),
   protocolMeta: getActiveProtocolMeta(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  openDialog: bindActionCreators(dialogActions.openDialog, dispatch),
+});
+
+const linkHandler = withHandlers({
+  handleClickStart: ({ hasUnsavedChanges, openDialog }) => () => {
+    const goToStartScreen = () => history.push('/');
+    if (!hasUnsavedChanges) { goToStartScreen(); return; }
+    openDialog({
+      type: 'Warning',
+      title: 'Unsaved changes',
+      message: (
+        <div>
+          Are you sure you want to go back to the start screen?
+          <p><strong>Any unsaved changes will be lost!</strong></p>
+        </div>
+      ),
+      confirmLabel: 'Go to start screen',
+      onConfirm: goToStartScreen,
+    });
+  },
 });
 
 export { Scene };
 
 export default compose(
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
+  linkHandler,
 )(Scene);
