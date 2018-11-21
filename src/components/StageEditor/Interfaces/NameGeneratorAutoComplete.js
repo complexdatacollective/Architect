abc32b871ee4f45581460d171349e9ca25201f7b
Steve Mckellar <steve@wwqrd.uk>
2018-11-21 17:45:36 +0000

    First pass at auto complete prompt

---
+++ src/components/StageEditor/Interfaces/NameGeneratorAutoComplete.js
@@ -0,0 +1,15 @@
+import {
+  Name,
+  NodeType,
+  NameGeneratorAutoCompletePrompts,
+} from '../sections';
+
+const NameGeneratorAutoComplete = {
+  sections: [
+    Name,
+    NodeType,
+    NameGeneratorAutoCompletePrompts,
+  ],
+};
+
+export default NameGeneratorAutoComplete;
