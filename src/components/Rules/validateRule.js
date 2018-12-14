423541bb1e3cd0b6dd32c0b78bcdf3a54b20f108
Steve Mckellar <steve@wwqrd.uk>
2018-12-14 14:59:57 +0000

    Fix filter validations

--- src/components/Rules/validateRule.js
+++ src/components/Rules/validateRule.js
@@ -1,4 +1,5 @@
 import { isEmpty } from 'lodash';
+import { operatorsWithValue } from './options';
 
 const validateFields = (fields = [], options = {}) =>
   fields.every(field => !isEmpty(options[field]));
@@ -9,7 +10,10 @@ const validateRule = (rule) => {
   switch (rule.type) {
     case 'alter': {
       if (Object.prototype.hasOwnProperty.call(options, 'variable')) {
-        return validateFields(['type', 'variable', 'operator', 'value'], options);
+        if (operatorsWithValue.has(options.operator)) {
+          return validateFields(['type', 'variable', 'operator', 'value'], options);
+        }
+        return validateFields(['type', 'variable', 'operator'], options);
       }
       return validateFields(['type', 'operator'], options);
     }
