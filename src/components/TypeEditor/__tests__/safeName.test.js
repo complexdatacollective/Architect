d7855c6eba185af8d54cbf08f920e85892a61633
Joshua Melville <joshua.melville@northwestern.edu>
2018-12-17 13:39:32 +0000

    update tests

--- src/components/TypeEditor/__tests__/safeName.test.js
+++ src/components/TypeEditor/__tests__/safeName.test.js
@@ -6,7 +6,7 @@ import safeName from '../safeName';
 const testString = range(32, 127).map(code => String.fromCharCode(code)).join();
 
 describe('safeName()', () => {
-  it('allows alphanumerics and underscore, and converts to lowercase', () => {
-    expect(safeName(testString)).toEqual('0123456789abcdefghijklmnopqrstuvwxyz_abcdefghijklmnopqrstuvwxyz');
+  it('allows alphanumerics, dashes, and underscore', () => {
+    expect(safeName(testString)).toEqual('-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz');
   });
 });
