74eea0ab8e2ac9af8d6b6ef53665643eb533d1a6
Steve Mckellar <steve@wwqrd.uk>
2019-01-21 16:06:22 +0000

    Add undo functionality to protocol object with keyboard shortcuts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import initReactFastclick from 'react-fastclick';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ActionCreators as undoActions } from 'redux-undo';
+import Mousetrap from 'mousetrap';
 import appHistory from './history';
 import { store, persistor } from './ducks/store';
 import App from './components/App';
@@ -16,6 +18,11 @@ import './styles/main.scss';
 
 initPreventFileDrop();
 initReactFastclick();
+
+const bindShortcuts = () => {
+  Mousetrap.bind('command+z', () => store.dispatch(undoActions.undo()));
+  Mousetrap.bind('command+shift+z', () => store.dispatch(undoActions.redo()));
+};
 
 const startApp = () => {
   ReactDOM.render(
@@ -42,4 +49,5 @@ const startApp = () => {
 window.addEventListener('load', () => {
   startApp();
   initFileOpener();
+  bindShortcuts();
 });
