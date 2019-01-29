9e07212b48c996c63c06e6162fa7a5c0523185bb
Steve Mckellar <steve@wwqrd.uk>
2019-01-29 15:54:21 +0000

    Fix preview text for 'type' alter rules

--- src/components/Rules/PreviewText.js
+++ src/components/Rules/PreviewText.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types';
 import { get } from 'lodash';
 
 const operatorsAsText = {
-  EXACTLY: 'exactly',
+  // IS: 'is',
+  // IS_NOT: 'is_not',
   EXISTS: 'with',
   NOT_EXISTS: 'without',
+  EXACTLY: 'exactly',
   NOT: 'is not',
   GREATER_THAN: 'greater than',
   GREATER_THAN_OR_EQUAL: 'greater than or equal to',
   LESS_THAN: 'less than',
   LESS_THAN_OR_EQUAL: 'less that or equal to',
+};
+
+const typeOperatorsAsText = {
+  EXISTS: 'is',
+  NOT_EXISTS: 'is not',
 };
 
 const formatValue = (value) => {
@@ -38,9 +45,21 @@ const Variable = ({ children }) => (<div className="rules-preview-text__variable
 Variable.propTypes = { children: PropTypes.node };
 Variable.defaultProps = { children: '' };
 
-const Operator = ({ value }) => (<div className="rules-preview-text__operator">{get(operatorsAsText, value, value.toLowerCase())}</div>);
+const Operator = ({ value }) => (
+  <div className="rules-preview-text__operator">
+    {get(operatorsAsText, value, value.toLowerCase())}
+  </div>
+);
 Operator.propTypes = { value: PropTypes.string };
 Operator.defaultProps = { value: '' };
+
+const TypeOperator = ({ value }) => (
+  <div className="rules-preview-text__operator">
+    {get(typeOperatorsAsText, value, value.toLowerCase())}
+  </div>
+);
+TypeOperator.propTypes = { value: PropTypes.string };
+TypeOperator.defaultProps = { value: '' };
 
 const Value = ({ value }) => {
   const formattedValue = formatValue(value);
@@ -67,10 +86,10 @@ const PreviewText = ({ type, options }) => {
       if (!options.variable) {
         return (
           <Fragment>
+            <TypeOperator value={options.operator} />
             <Entity>alter</Entity>
             <Copy>of type</Copy>
             <Type>{options.type}</Type>
-            <Operator value={options.operator} />
           </Fragment>
         );
       }
