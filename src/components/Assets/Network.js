af5484910b8b790f6dd6053ec75b819d6d4f1ca5
Steve Mckellar <steve@wwqrd.uk>
2019-02-14 14:23:15 +0000

    with styled network asset

---
+++ src/components/Assets/Network.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Icon from '../../ui/components/Icon';
+import withAssetMeta from './withAssetMeta';
+
+const Network = ({ id, meta }) => (
+  <div className={cx('assets-network', { 'assets-network--existing': id === 'existing' })}>
+    <div className="assets-network__icon"><Icon name="links" /></div>
+    <div className="assets-network__label">
+      {meta.name}
+    </div>
+  </div>
+);
+
+Network.propTypes = {
+  id: PropTypes.string.isRequired,
+  meta: PropTypes.object,
+};
+
+Network.defaultProps = {
+  meta: {
+    name: '',
+  },
+};
+
+export { Network };
+
+export default withAssetMeta(Network);
--- src/components/Form/Fields/DataSource.js
+++ src/components/Form/Fields/DataSource.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { fieldPropTypes } from 'redux-form';
+import Checkbox from '../../../ui/components/Fields/Checkbox';
+import NetworkAsset from '../../Assets/Network';
 import File from './File';
 
-const DataSource = props => (
-  <File
-    type="network"
-    {...props}
-  >
-    { id => (
-      <div className="form-fields-network-file">
-        {id}
+class DataSource extends Component {
+  handleClickCheckbox = () => {
+    if (this.props.input.value === 'existing') { return; }
+    this.props.input.onChange('existing');
+  }
+
+  render() {
+    const {
+      useExisting,
+      input,
+    } = this.props;
+
+    const checkboxInput = {
+      value: input.value === 'existing',
+      onChange: this.handleClickCheckbox,
+    };
+
+    return (
+      <div>
+        { useExisting &&
+          <Checkbox
+            label="Use network from interview"
+            input={checkboxInput}
+          />
+        }
+
+        <File
+          type="network"
+          {...this.props}
+        >
+          { id => <NetworkAsset id={id} /> }
+        </File>
       </div>
-    ) }
-  </File>
-);
+    );
+  }
+}
 
 DataSource.propTypes = {
   ...fieldPropTypes,
+  useExisting: PropTypes.bool,
+};
+
+DataSource.defaultProps = {
+  useExisting: true,
 };
 
 export default DataSource;
