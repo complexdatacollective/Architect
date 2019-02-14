e35f2d3226aea381463f3239f586311ccba2b02d
Steve Mckellar <steve@wwqrd.uk>
2019-02-14 13:21:38 +0000

    Update all instances of dataSource/externalData to use file input

--- src/components/StageEditor/sections/NameGeneratorListPrompts/index.js
+++ src/components/StageEditor/sections/NameGeneratorListPrompts/index.js
@@ -1,4 +1,3 @@
 export { default } from './NameGeneratorListPrompts';
 export { default as optionGetters } from './optionGetters';
 export { default as withFieldValues } from './withFieldValues';
-export { default as withDataSourceOptions } from './withDataSourceOptions';
--- src/components/StageEditor/sections/NodePanels/NodePanel.js
+++ src/components/StageEditor/sections/NodePanels/NodePanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
 import * as Fields from '../../../../ui/components/Fields';
-import Select from '../../../Form/Fields/Select';
+import DataSource from '../../../Form/Fields/DataSource';
 import { Filter, withFieldConnector, withStoreConnector } from '../../../Query';
 import ValidatedField from '../../../Form/ValidatedField';
 import { Item, Row } from '../../../OrderedList';
@@ -10,18 +10,7 @@ import { getFieldId } from '../../../../utils/issues';
 
 const FilterField = withFieldConnector(withStoreConnector(Filter));
 
-const getDataSourceOptions = (dataSources) => {
-  const externalData = dataSources.map(dataSource => (
-    { value: dataSource, label: dataSource }
-  ));
-
-  return ([
-    { value: 'existing', label: 'Current network' },
-    ...externalData,
-  ]);
-};
-
-const NodePanel = ({ fieldId, dataSources, ...rest }) => (
+const NodePanel = ({ fieldId, ...rest }) => (
   <Item {...rest}>
     <Row>
       <h3>Panel title</h3>
@@ -37,11 +26,9 @@ const NodePanel = ({ fieldId, dataSources, ...rest }) => (
         data-name="Panel data source"
       >Data source</h3>
       <ValidatedField
+        component={DataSource}
         name={`${fieldId}.dataSource`}
-        component={Select}
-        placeholder="&mdash; Select data source &mdash;"
         validation={{ required: true }}
-        options={getDataSourceOptions(dataSources)}
       />
     </Row>
     <Row>
@@ -56,7 +43,6 @@ const NodePanel = ({ fieldId, dataSources, ...rest }) => (
 
 NodePanel.propTypes = {
   fieldId: PropTypes.string.isRequired,
-  dataSources: PropTypes.array.isRequired,
 };
 
 export { NodePanel };
