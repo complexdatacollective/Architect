08fcffb8232769b4db2074a264fe98395930633b
Steve Mckellar <steve@wwqrd.uk>
2019-03-08 10:55:45 +0000

    Narrative behaviours properties should be grouped

--- src/components/StageEditor/sections/NarrativeBehaviours.js
+++ src/components/StageEditor/sections/NarrativeBehaviours.js
@@ -1,30 +1,30 @@
 import React from 'react';
+import { FormSection } from 'redux-form';
 import Guidance from '../../Guidance';
 import { ValidatedField } from '../../../components/Form';
 import * as Fields from '../../../ui/components/Fields';
 import { getFieldId } from '../../../utils/issues';
 
-// "freeDraw": true,
-// "allowRepositioning": true
-
 const NarrativeBehaviours = () => (
   <Guidance contentId="guidance.editor.name">
     <div className="stage-editor-section">
-      <div id={getFieldId('freeDraw')} data-name="Free draw" />
-      <h2>Free-draw</h2>
-      <ValidatedField
-        name="freeDraw"
-        label="Allow drawing on the canvas."
-        component={Fields.Checkbox}
-      />
+      <FormSection name="behaviours">
+        <div id={getFieldId('freeDraw')} data-name="Free draw" />
+        <h2>Free-draw</h2>
+        <ValidatedField
+          name="behaviours.freeDraw"
+          label="Allow drawing on the canvas."
+          component={Fields.Checkbox}
+        />
 
-      <div id={getFieldId('allowRepositioning')} data-name="Allow repositioning" />
-      <h2>Allow repositioning</h2>
-      <ValidatedField
-        name="allowRepositioning"
-        label="Allow repositioning of nodes."
-        component={Fields.Checkbox}
-      />
+        <div id={getFieldId('allowRepositioning')} data-name="Allow repositioning" />
+        <h2>Allow repositioning</h2>
+        <ValidatedField
+          name="behaviours.allowRepositioning"
+          label="Allow repositioning of nodes."
+          component={Fields.Checkbox}
+        />
+      </FormSection>
     </div>
   </Guidance>
 );
