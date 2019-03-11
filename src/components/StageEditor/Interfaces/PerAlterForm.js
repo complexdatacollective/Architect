66630f469b63a3b131679dea78e38848edc415be
Steve Mckellar <steve@wwqrd.uk>
2019-03-11 10:47:22 +0000

    Per Alter Form interface

---
+++ src/components/StageEditor/Interfaces/PerAlterForm.js
@@ -0,0 +1,15 @@
+import {
+  Name,
+  NodeType,
+  Form,
+} from '../sections';
+
+const NameGenerator = {
+  sections: [
+    Name,
+    NodeType,
+    Form,
+  ],
+};
+
+export default NameGenerator;
