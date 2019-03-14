3f7d9efdcccb6770b3cab9e66f36659aef7c77ba
Steve Mckellar <steve@wwqrd.uk>
2019-03-14 14:48:18 +0000

    Fix web shims for fs-extra

--- src/__mocks__/fs-extra.js
+++ src/__mocks__/fs-extra.js
@@ -1,7 +1,11 @@
 /* eslint-env jest */
 
 const copySync = jest.fn();
+const emptyDirSync = jest.fn();
+const readJson = jest.fn(() => Promise.resolve());
 
 export default {
   copySync,
+  emptyDirSync,
+  readJson,
 };
--- src/utils/webShims/fs-extra.js
+++ src/utils/webShims/fs-extra.js
@@ -1,7 +1,22 @@
 /* eslint-disable */
 
+const mockProtocol = require('./mockProtocol.json');
+
 const copySync = () => {};
+const emptyDirSync = () => {};
+const readJson = (...args) => {
+  const filename = args.shift();
+
+  switch(true) {
+    case /\.json$/.test(filename):
+      return Promise.resolve(mockProtocol);
+    default:
+      return Promise.resolve({});
+  }
+}
 
 module.exports = {
   copySync,
+  emptyDirSync,
+  readJson,
 };
