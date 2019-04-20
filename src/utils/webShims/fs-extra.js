c04aeff19aeb65a340b5691937ea06c051758169
Steve Mckellar <steve@wwqrd.uk>
2019-04-20 13:11:24 +0100

    fix webshims for external data

--- src/utils/webShims/fs-extra.js
+++ src/utils/webShims/fs-extra.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 
 const mockProtocol = require('./mockProtocol.json');
+const mockExternalJSONData = require('./mockExternalData.json');
+const mockExternalCSVData = "Name,Nickname,Age\nAnita,Annie,21\nBarry,Baz,28\nCarlito,Carl,23\nDee,Dee,40\nEugine,Eu,18\n";
 
 const copySync = () => {};
 const emptyDirSync = () => {};
@@ -8,10 +10,23 @@ const readJson = (...args) => {
   const filename = args.shift();
 
   switch(true) {
-    case /\.json$/.test(filename):
+    case /protocol\.json$/.test(filename):
       return Promise.resolve(mockProtocol);
+    case /\.json$/.test(filename):
+      return Promise.resolve(mockExternalJSONData);
     default:
       return Promise.resolve({});
+  }
+};
+
+const readFile = (...args) => {
+  const filename = args.shift();
+
+  switch(true) {
+      case /\.csv$/.test(filename):
+      return Promise.resolve(mockExternalCSVData);
+    default:
+      return Promise.resolve('');
   }
 };
 
@@ -19,4 +34,5 @@ module.exports = {
   copySync,
   emptyDirSync,
   readJson,
+  readFile,
 };
