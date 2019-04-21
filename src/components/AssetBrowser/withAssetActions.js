601db4105586823697d1a8e1fce8b94c8156e66c
Steve Mckellar <steve@wwqrd.uk>
2019-04-21 18:27:24 +0100

    Dialog box to confirm delete asset

--- src/components/AssetBrowser/withAssetActions.js
+++ src/components/AssetBrowser/withAssetActions.js
@@ -4,23 +4,32 @@ import {
   withHandlers,
 } from 'recompose';
 import { actionCreators as assetActions } from '../../ducks/modules/protocol/assetManifest';
+import { actionCreators as dialogActions } from '../../ducks/modules/dialogs';
 
-const assetStore = connect(
+const connectActions = connect(
   null,
   {
     deleteAsset: assetActions.deleteAsset,
+    openDialog: dialogActions.openDialog,
   },
 );
 
 const assetHandlers = withHandlers({
-  onDelete: ({ deleteAsset }) =>
+  onDelete: ({ deleteAsset, openDialog }) =>
     (assetId) => {
-      deleteAsset(assetId);
+      openDialog({
+        id: '1234-1234-1',
+        type: 'Confirm',
+        title: 'Delete Asset',
+        message: 'Are you sure you want to delete this asset?',
+        confirmLabel: 'Delete asset',
+        onConfirm: () => deleteAsset(assetId),
+      });
     },
 });
 
 const withAssets = compose(
-  assetStore,
+  connectActions,
   assetHandlers,
 );
 
