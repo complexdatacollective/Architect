a7f1fe74bfb76efe9fca6f80eee5628d0475f1d6
Steve Mckellar <steve@wwqrd.uk>
2019-04-26 11:24:07 +0100

    Remove defunt styles and move NewButton into Timeline

--- src/components/OrderedList/index.js
+++ src/components/OrderedList/index.js
@@ -3,6 +3,5 @@ export { default as Item } from './Item';
 export { default as Row } from './Row';
 export { default as Group } from './Group';
 export { default as ExpandableItem } from './ExpandableItem';
-export { default as NewButton } from './NewButton';
 export { default as Handle } from './Handle';
 export { default as DeleteButton } from './DeleteButton';
