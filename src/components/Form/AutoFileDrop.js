8ac140bc0af3f2c09bcb7cdf7638617ced1476e1
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-07 22:56:18 +0100

    attempt to use file extension to look up type on windows

--- src/components/Form/AutoFileDrop.js
+++ src/components/Form/AutoFileDrop.js
@@ -1,29 +1,45 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { findKey } from 'lodash';
 import { compose, withProps, withHandlers } from 'recompose';
 import Dropzone from './Dropzone';
 import { actionCreators as assetActions } from '../../ducks/modules/protocol/assetManifest';
 
 const ACCEPTS = {
-  network: ['text/csv', 'application/json'],
+  network: ['text/csv', 'application/json', 'text/*,', 'text/*', '.csv'], // text/*, is a workaround for windows not correctly setting mime type for CSV.
   image: ['image/*'],
   audio: ['audio/*'],
   video: ['video/*'],
 };
 
-const TYPES = [
-  [/text\/csv/, 'network'],
-  [/application\/json/, 'network'],
-  [/image\/.*/, 'image'],
-  [/audio\/.*/, 'audio'],
-  [/video\/.*/, 'video'],
-];
-
 const getTypeFromMime = (mime) => {
-  const match = TYPES.find(([matcher]) => matcher.test(mime));
+  const MIME_TYPES = [
+    [/text\/csv/, 'network'],
+    [/application\/json/, 'network'],
+    [/text\/*,/, 'network'],
+    [/image\/.*/, 'image'],
+    [/audio\/.*/, 'audio'],
+    [/video\/.*/, 'video'],
+  ];
+
+  const match = MIME_TYPES.find(([matcher]) => matcher.test(mime));
   if (!match) { return null; }
   return match[1];
 };
+
+const getTypeFromExtension = (name) => {
+  const EXTENSION_TYPES = {
+    network: ['csv', 'json'], // .csv
+    image: ['jpg', 'jpeg', 'gif', 'png'],
+    audio: ['mp3', 'aiff'],
+    video: ['mov', 'mp4'],
+  };
+
+  const extension = name.split('.').pop();
+
+  return findKey(EXTENSION_TYPES, type => type.includes(extension));
+};
+
 
 const mapDispatchToProps = dispatch => ({
   importAsset: bindActionCreators(assetActions.importAsset, dispatch),
@@ -42,7 +58,7 @@ const autoFileDrop = compose(
     onDrop: ({ importAsset, onDrop }) =>
       (acceptedFiles) => {
         acceptedFiles.forEach((file) => {
-          const type = getTypeFromMime(file.type);
+          const type = getTypeFromMime(file.type) || getTypeFromExtension(file.name);
 
           importAsset(file, type)
             .then(({ id }) => {
