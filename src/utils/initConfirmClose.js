3f958464263825b299cc4ce27fe07173268a9412
Steve Mckellar <steve@wwqrd.uk>
2019-05-24 12:21:37 +0100

    Confirm close only when unsaved changes

--- src/utils/initConfirmClose.js
+++ src/utils/initConfirmClose.js
@@ -1,15 +1,29 @@
+import React from 'react';
 import { ipcRenderer } from 'electron';
 import { store } from '../ducks/store';
 import { actionCreators as dialogActions } from '../ducks/modules/dialogs';
 
 const initFileOpener = () => {
   ipcRenderer.on('CONFIRM_CLOSE', () => {
+    const state = store.getState();
+    const hasUnsavedChanges = state.session.lastChanged > state.session.lastSaved;
+
+    if (!hasUnsavedChanges) {
+      ipcRenderer.send('QUIT');
+      return;
+    }
+
     store.dispatch(
       dialogActions.openDialog({
-        type: 'Confirm',
-        title: 'Are you really sure that you want to do this?',
-        message: 'Like really really sure?',
-        confirmLabel: 'Yes please!',
+        type: 'Warning',
+        title: 'Unsaved changes',
+        message: (
+          <div>
+            Are you sure you want to exit the application?
+            <p><strong>Any unsaved changes will be lost!</strong></p>
+          </div>
+        ),
+        confirmLabel: 'Exit application',
         onConfirm: () => {
           ipcRenderer.send('QUIT');
         },
