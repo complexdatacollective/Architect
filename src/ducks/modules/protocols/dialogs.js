052a0ad9a7c3c7cd964c88d7f0bd186862a1566e
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-30 13:50:47 +0100

    update protocol validation and dialog text

--- src/ducks/modules/protocols/dialogs.js
+++ src/ducks/modules/protocols/dialogs.js
@@ -5,10 +5,14 @@ import { actionCreators as dialogActions } from '../dialogs';
 export const validationErrorDialog = (e) => {
   e.friendlyMessage = (
     <React.Fragment>
-      <p>Not a valid protocol.</p>
       <p>
-        You may still save and edit the protocol but<br />
-        it <strong>will not be compatable with Network Canvas or Server</strong>.
+        The protocol format seems to be invalid. Please help us to troubleshoot this issue
+        by sharing your protocol file (or the steps to reproduce this problem) with us by
+        emailing <code>feedback@networkcanvas.com</code>.
+      </p>
+      <p>
+        You may still save and edit the protocol but it <strong>will not be compatable with
+        Network Canvas or Server</strong>.
       </p>
     </React.Fragment>
   );
@@ -21,9 +25,10 @@ export const validationErrorDialog = (e) => {
 
 export const saveErrorDialog = (e, filePath) => {
   e.friendlyMessage = (
-    <React.Fragment>
-      <em>{path.basename(filePath)}</em> could not be saved.
-    </React.Fragment>
+    <p>
+      <em>{path.basename(filePath)}</em> could not be saved. See
+      the information below for details about why this error occured.
+    </p>
   );
 
   return dialogActions.openDialog({
