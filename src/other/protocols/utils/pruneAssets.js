c42c7ab56a7f3fd92a894a6311df47cb72770bcb
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-30 14:44:41 +0100

    fix isInTempPath on windows

--- src/other/protocols/utils/pruneAssets.js
+++ src/other/protocols/utils/pruneAssets.js
@@ -6,16 +6,17 @@ const contains = (protocol, string) =>
   protocol.indexOf(string) !== -1;
 
 const isInTempPath = (filePath) => {
-  console.log('isin temp paath', filePath);
   const tempPath = remote.app.getPath('temp');
 
-  return new RegExp(`^${tempPath}`)
-    .test(path.normalize(filePath));
+  return path.normalize(filePath).indexOf(tempPath) === 0;
 };
 
 const removeFile = (filePath) => {
   // Check we haven't somehow ended up outside of temppath since we are deleting things
-  if (!isInTempPath(filePath)) { throw new Error('Aborted remove because outside of temp'); }
+  if (!isInTempPath(filePath)) {
+    const tempPath = remote.app.getPath('temp');
+    throw new Error(`File ${filePath}) could not be removed because it is outside of the temporary folder (${tempPath})`);
+  }
 
   fs.unlinkSync(filePath);
 };
