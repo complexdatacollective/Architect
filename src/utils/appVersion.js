67ff183248f7d7dac7ef46c13f60a9ef1ced4e88
Joshua Melville <joshua.melville@northwestern.edu>
2019-06-16 21:14:08 +0100

    correct app versions

--- src/utils/appVersion.js
+++ src/utils/appVersion.js
@@ -2,16 +2,17 @@ import { remote } from 'electron';
 import { get } from 'lodash';
 
 const codenames = {
-  '4.0.1-alpha.2': 'Anning',
-  '4.0.1-alpha.3': 'Hercules',
-  '4.0.1-alpha.4': 'Gold-Bug',
+  '4.0.0-alpha.2': 'Anning',
+  '4.0.0-alpha.3': 'Hercules',
+  '4.0.0-alpha.4': 'Gold-Bug',
   // Skip versions to sync with Network Canvas
-  '4.0.1-alpha.8': 'Lochs & Glens',
-  '4.0.1-alpha.9': 'Arrakis',
-  '4.0.1-alpha.10': 'Porthleven',
-  '4.0.1-alpha.11': 'Cascades',
-  '4.0.1-alpha.12': 'No Coffee',
-  '4.0.1-beta.1': '',
+  '4.0.0-alpha.8': 'Lochs & Glens',
+  '4.0.0-alpha.9': 'Arrakis',
+  '4.0.0-alpha.10': 'Porthleven',
+  '4.0.0-alpha.11': 'Cascades',
+  '4.0.0-alpha.12': 'No Coffee',
+  '4.0.0-beta.1': '',
+  '4.0.0': 'Beta 1',
   '4.0.1': 'Beta 1',
 };
 
