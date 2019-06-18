7b5a58c6431a2c1ebe3ef8fb8c1fe9a17eb7137d
Steve Mckellar <steve@wwqrd.uk>
2019-06-18 15:58:50 +0100

    Filter types for AssignAttributes variableOptions

--- src/components/AssignAttributes/withVariableOptions.js
+++ src/components/AssignAttributes/withVariableOptions.js
@@ -2,12 +2,21 @@ import { connect } from 'react-redux';
 import { formValueSelector } from 'redux-form';
 import { getVariableOptionsForSubject } from '../../selectors/codebook';
 
+const ALLOWED_TYPES = [
+  'text',
+  'number',
+  'boolean',
+  'ordinal',
+  'categorical',
+];
+
 const mapStateToProps = (state, { entity, type, form, fields }) => {
   const usedVariables = (formValueSelector(form)(state, fields.name) || [])
     .map(({ variable }) => variable);
   const variableOptions = getVariableOptionsForSubject(state, { entity, type });
 
   const variableOptionsWithUsedDisabled = variableOptions
+    .filter(({ type: optionType }) => ALLOWED_TYPES.includes(optionType))
     .map(({ value, ...rest }) => ({
       ...rest,
       value,
