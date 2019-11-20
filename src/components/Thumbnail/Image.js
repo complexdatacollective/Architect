69e73b9a47a8d0c5302e679e677d0d770bf174ef
Steve Mckellar <steve@wwqrd.uk>
2019-11-20 13:53:12 +0000

    Tidy up ImageThumbnail

--- src/components/Thumbnail/Image.js
+++ src/components/Thumbnail/Image.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import withAssetUrl from '@components/Assets/withAssetUrl';
 
-const backgroundStyles = ({ url, contain }) => ({
-  backgroundImage: `url(${url})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: contain ? 'contain' : 'cover',
-  backgroundPosition: 'center',
-});
+const ImageThumbnail = ({ url, contain, dispatch, ...props }) => {
+  const className = cx(
+    'thumbnail',
+    'thumbnail--image',
+    { 'thumbnail--contain': contain },
+  );
 
-const ImageThumbnail = ({ url, contain, dispatch, ...props }) => (
-  <div
-    className="thumbnail thumbnail--image"
-    style={backgroundStyles({ url, contain })}
-    {...props}
-  />
-);
+  return (
+    <div
+      className={className}
+      style={{ backgroundImage: `url(${url})` }}
+      {...props}
+    />
+  );
+};
 
 ImageThumbnail.propTypes = {
   url: PropTypes.string.isRequired,
-  contain: PropTypes.boolean,
+  contain: PropTypes.bool,
 };
 
 ImageThumbnail.defaultProps = {
