490f623deba01f32e1776849395cff3acf862ecf
Steve Mckellar <steve@wwqrd.uk>
2020-04-23 15:35:27 +0100

    Nice save animation

--- src/components/ProtocolControlBar.js
+++ src/components/ProtocolControlBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 import { Button, Icon, Spinner } from '@codaco/ui';
 import { getProtocol } from '@selectors/protocol';
 import { actionCreators as protocolsActions, actionLocks as protocolsLocks } from '@modules/protocols';
@@ -9,31 +10,47 @@ import ControlBar from './ControlBar';
 
 const RightArrow = <Icon name="arrow-right" />;
 
+const labelVariants = {
+  ready: { opacity: 1 },
+  busy: { opacity: 0 },
+};
+
+const spinnerVariants = {
+  ready: { opacity: 0 },
+  busy: { opacity: 1 },
+};
+
 const ProtocolControlBar = ({
   saveProtocol,
-  hasUnsavedChanges,
-  hasAnyStages,
   show,
+  showControlBar,
+  isDisabled,
   isSaving,
 }) => {
   const saveProps = !isSaving &&
-    {
-      icon: RightArrow,
-      iconPosition: 'right',
-    };
+  {
+    icon: RightArrow,
+    iconPosition: 'right',
+  };
+
   return (
     <ControlBar
-      show={show && hasUnsavedChanges}
+      show={show && showControlBar}
       buttons={[
         <Button
           onClick={saveProtocol}
           color="white"
           data-variant="save"
-          disabled={!hasAnyStages || isSaving}
+          disabled={isDisabled}
           {...saveProps}
         >
-          {!isSaving && 'Save'}
-          {isSaving && <Spinner /> }
+          <motion.div
+            initial="ready"
+            animate={isSaving ? 'busy' : 'ready'}
+          >
+            <motion.div variants={labelVariants}>Save</motion.div>
+            <motion.div className="button__busy" variants={spinnerVariants}><Spinner /></motion.div>
+          </motion.div>
         </Button>,
       ]}
     />
@@ -42,9 +59,9 @@ const ProtocolControlBar = ({
 
 ProtocolControlBar.propTypes = {
   saveProtocol: PropTypes.func.isRequired,
-  hasUnsavedChanges: PropTypes.bool.isRequired,
-  hasAnyStages: PropTypes.bool.isRequired,
   isSaving: PropTypes.bool.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+  showControlBar: PropTypes.bool.isRequired,
   show: PropTypes.bool,
 };
 
@@ -52,14 +69,19 @@ ProtocolControlBar.defaultProps = {
   show: true,
 };
 
-const mapStateToProps = state => ({
-  hasUnsavedChanges: (
-    state.session.lastChanged >
-    state.session.lastSaved
-  ),
-  hasAnyStages: getProtocol(state).stages.length > 0,
-  isSaving: statusSelectors.getIsBusy(state, protocolsLocks.saving),
-});
+const mapStateToProps = (state) => {
+  const hasUnsavedChanges = state.session.lastChanged > state.session.lastSaved;
+  const hasAnyStages = getProtocol(state).stages.length > 0;
+  const isSaving = statusSelectors.getIsBusy(state, protocolsLocks.saving);
+  const isDisabled = !hasAnyStages || isSaving;
+  const showControlBar = hasUnsavedChanges || isSaving;
+
+  return {
+    isSaving,
+    isDisabled,
+    showControlBar,
+  };
+};
 
 const mapDispatchToProps = {
   saveProtocol: protocolsActions.saveAndBundleProtocol,
