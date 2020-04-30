4bbd3dbd31819e12b160d2352e5a027691e4cce8
Steve Mckellar <steve@wwqrd.uk>
2020-04-30 12:22:31 +0100

    Pretty tabs on StageEditor

--- src/components/StageEditor/StageEditor.js
+++ src/components/StageEditor/StageEditor.js
@@ -1,96 +1,110 @@
-import React, { Component } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { ipcRenderer } from 'electron';
 import PropTypes from 'prop-types';
 import { compose, defaultProps } from 'recompose';
 import cx from 'classnames';
+import { motion } from 'framer-motion';
 import Editor from '@components/Editor';
 import { getInterface } from './Interfaces';
 import withStageEditorHandlers from './withStageEditorHandlers';
 import withStageEditorMeta from './withStageEditorMeta';
 import StageHeading from './StageHeading';
-import Tabs from './Tabs';
 
 const formName = 'edit-stage';
 
-class StageEditor extends Component {
-  componentDidMount() {
-    ipcRenderer.on('REFRESH_PREVIEW', this.handleRefresh);
-  }
+const tabVariants = {
+  active: { opacity: 1, height: 'auto', position: 'static' },
+  inactive: { opacity: 0, height: 0, position: 'absolute', top: 0 },
+};
 
-  componentWillUnmount() {
-    ipcRenderer.removeListener('REFRESH_PREVIEW', this.handleRefresh);
-  }
+const StageEditor = ({
+  id,
+  previewStage,
+  interfaceType,
+  stagePath,
+  ...props
+}) => {
+  const [tab, setTab] = useState(0);
 
-  sections() {
-    return getInterface(this.props.interfaceType).sections;
-  }
+  useEffect(() => {
+    ipcRenderer.on('REFRESH_PREVIEW', previewStage);
 
-  name() {
-    return getInterface(this.props.interfaceType).name || this.props.interfaceType;
-  }
+    return () =>
+      ipcRenderer.removeListener('REFRESH_PREVIEW', previewStage);
+  }, []);
 
-  handleRefresh = () => {
-    this.props.previewStage();
-  }
+  const sections = useMemo(
+    () => getInterface(interfaceType).sections,
+    [interfaceType],
+  );
 
-  renderSections({ submitFailed, windowRoot }) {
-    return this.sections().map((SectionComponent, index) => (
+  const renderSections = ({ submitFailed, windowRoot }) =>
+    sections.map((SectionComponent, index) => (
       <SectionComponent
         key={index}
         form={formName}
-        stagePath={this.props.stagePath}
+        stagePath={stagePath}
         hasSubmitFailed={submitFailed}
         // `windowRoot` will ensure connect() components re-render
         // when the window root changes
         windowRoot={windowRoot}
-        interfaceType={this.props.interfaceType}
+        interfaceType={interfaceType}
       />
     ));
-  }
 
-  render() {
-    const panelClasses = selected => cx(
-      'stage-editor__panel',
-      { 'stage-editor__panel--selected': selected },
-    );
+  const panelClasses = selected => cx(
+    'stage-editor__panel',
+    { 'stage-editor__panel--selected': selected },
+  );
 
-    const tabClasses = selected => cx(
-      'stage-editor__tab',
-      { 'stage-editor__tab--selected': selected },
-    );
+  const tabClasses = selected => cx(
+    'stage-editor__tab',
+    { 'stage-editor__tab--selected': selected },
+  );
 
-    return (
-      <Editor
-        formName={formName}
-        {...this.props}
-      >
-        {
-          ({ submitFailed, windowRoot }) => (
-            <div className="stage-editor">
-              <StageHeading id={this.props.id} />
-              <Tabs>
-                {({ selectedTab, selectTab }) => (
-                  <div className="stage-editor__tabs">
-                    <div className="stage-editor__tablist">
-                      <div className={tabClasses(selectedTab === 0)} onClick={() => selectTab(0)}>Stage</div>
-                      <div className={tabClasses(selectedTab === 1)} onClick={() => selectTab(1)}>Skip Logic</div>
-                    </div>
-                    <div className={panelClasses(selectedTab === 0)}>
-                      {this.renderSections({ submitFailed, windowRoot })}
-                    </div>
-                    <div className={panelClasses(selectedTab === 1)}>
-                      skip logic
-                    </div>
-                  </div>
-                )}
-              </Tabs>
+  return (
+    <Editor
+      formName={formName}
+      {...props}
+    >
+      {
+        ({ submitFailed, windowRoot }) => (
+          <div className="stage-editor">
+            <StageHeading id={id} />
+            <div className="stage-editor__tabs">
+              <div className="stage-editor__tablist">
+                <div
+                  className={tabClasses(tab === 0)}
+                  onClick={() => setTab(0)}
+                >Stage</div>
+                <div
+                  className={tabClasses(tab === 1)}
+                  onClick={() => setTab(1)}
+                >Skip logic</div>
+              </div>
+              <div className="stage-editor__panels">
+                <motion.div
+                  className={panelClasses()}
+                  variants={tabVariants}
+                  animate={tab === 0 ? 'active' : 'inactive'}
+                >
+                  {renderSections({ submitFailed, windowRoot })}
+                </motion.div>
+                <motion.div
+                  className={panelClasses()}
+                  variants={tabVariants}
+                  animate={tab === 1 ? 'active' : 'inactive'}
+                >
+                  skip logic
+                </motion.div>
+              </div>
             </div>
-          )
-        }
-      </Editor>
-    );
-  }
-}
+          </div>
+        )
+      }
+    </Editor>
+  );
+};
 
 StageEditor.propTypes = {
   interfaceType: PropTypes.string.isRequired,
--- src/components/StageEditor/Tabs.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const Tabs = ({ children }) => {
-  const [state, setState] = useState(0);
-
-  return children({ selectedTab: state, selectTab: setState });
-};
-
-export default Tabs;
