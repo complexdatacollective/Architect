b9be22e55b70297879d71cfdcf4519c61baf58a8
Steve Mckellar <steve@wwqrd.uk>
2020-05-01 13:57:23 +0100

    Refactor SkipLogic panel into ContextPanel

---
+++ src/components/ContextPanel.js
@@ -0,0 +1,72 @@
+import React, { useMemo, useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import Checkbox from '@codaco/ui/lib/components/Fields/Checkbox';
+
+const ContextPanel = ({
+  title,
+  children,
+  isActive,
+  onDeactivate,
+}) => {
+  const panelVariants = useMemo(() => ({
+    activated: { height: '100%', opacity: 1 },
+    unactivated: { height: 0, opacity: 0 },
+  }), []);
+
+  const [open, setOpen] = useState(isActive);
+
+  const handleActivate = useCallback(() => {
+    if (!open) {
+      setOpen(true);
+      return;
+    }
+
+    onDeactivate()
+      .then((confirm) => {
+        if (confirm) { setOpen(false); }
+      });
+  }, [onDeactivate, open, setOpen]);
+
+  const variant = open ? 'activated' : 'unactivated';
+
+  return (
+    <motion.div
+      initial="unactivated"
+      animate={variant}
+      className="context-drawer"
+    >
+      <motion.div
+        className="context-drawer__controls"
+      >
+        <Checkbox
+          input={{ value: open }}
+          onClick={handleActivate}
+        />
+        {title}
+      </motion.div>
+      <motion.div
+        className="context-drawer__panel"
+        variants={panelVariants}
+      >
+        <div className="context-drawer__panel-container">
+          {children}
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+ContextPanel.propTypes = {
+  title: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  children: PropTypes.node,
+  onDeactivate: PropTypes.func,
+};
+
+ContextPanel.defaultProps = {
+  onDeactivate: () => Promise.resolve(),
+  children: null,
+};
+
+export default ContextPanel;
--- src/components/StageEditor/SkipLogic.js
+++ src/components/StageEditor/SkipLogic.js
@@ -1,10 +1,10 @@
-import React, { useMemo, useCallback, useState, useEffect } from 'react';
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { change as changeField, getFormValues } from 'redux-form';
+import { change, getFormValues } from 'redux-form';
 import { has } from 'lodash';
-import { motion } from 'framer-motion';
-import Checkbox from '@codaco/ui/lib/components/Fields/Checkbox';
 import { actionCreators as dialogActions } from '@modules/dialogs';
+import ContextPanel from '@components/ContextPanel';
 import SkipLogicFields from '@components/SkipLogicEditor/SkipLogicFields';
 
 const SkipLogic = ({
@@ -12,62 +12,35 @@ const SkipLogic = ({
   changeField,
   openDialog,
 }) => {
-  const [open, setOpen] = useState(hasSkipLogic);
-
-  const panelVariants = useMemo(() => ({
-    activated: { height: '100%', opacity: 1 },
-    unactivated: { height: 0, opacity: 0 },
-  }), []);
-
-  const resetSkipLogic = () => {
-    changeField('edit-stage', 'skipLogic', {});
-    setOpen(false);
-  };
-
-  const handleActivate = useCallback(() => {
-    if (!open) {
-      setOpen(true);
-      return;
-    }
-
-    openDialog({
-      type: 'Warning',
-      title: 'This will clear skip logic',
-      confirmLabel: 'Clear skip logic',
-    })
-      .then((confirm) => {
-        if (!confirm) { return; }
-        resetSkipLogic();
-      });
-  }, [openDialog, open, setOpen]);
-
-  const variant = open ? 'activated' : 'unactivated';
+  const handleDeactivate = useCallback(
+    () =>
+      openDialog({
+        type: 'Warning',
+        title: 'This will clear skip logic',
+        confirmLabel: 'Clear skip logic',
+      })
+        .then((confirm) => {
+          changeField('edit-stage', 'skipLogic', {});
+          return confirm;
+        }),
+    [openDialog, changeField],
+  );
 
   return (
-    <motion.div
-      initial="unactivated"
-      animate={variant}
-      className="context-drawer"
+    <ContextPanel
+      title="Skip Logic"
+      isActive={hasSkipLogic}
+      onDeactivate={handleDeactivate}
     >
-      <motion.div
-        className="context-drawer__controls"
-      >
-        <Checkbox
-          input={{ value: open }}
-          onClick={handleActivate}
-        />
-        Skip Logic
-      </motion.div>
-      <motion.div
-        className="context-drawer__panel"
-        variants={panelVariants}
-      >
-        <div className="context-drawer__panel-container">
-          <SkipLogicFields />
-        </div>
-      </motion.div>
-    </motion.div>
+      <SkipLogicFields />
+    </ContextPanel>
   );
+};
+
+SkipLogic.propTypes = {
+  hasSkipLogic: PropTypes.bool.isRequired,
+  changeField: PropTypes.func.isRequired,
+  openDialog: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -76,7 +49,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   openDialog: dialogActions.openDialog,
-  changeField,
+  changeField: change,
 };
 
 export { SkipLogic };
