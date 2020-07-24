69d7f6e63b9e3a808cf7d73b47fd96fc98370e09
Steve Mckellar <steve@wwqrd.uk>
2020-07-24 12:34:52 +0100

    Splash component

---
+++ src/components/Splash.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Version from '@components/Version';
+import architectIcon from '@app/images/architect-logo-icon.svg';
+import networkCanvasLogo from '@app/images/network-canvas-brand.svg';
+
+const Splash = () => (
+  <motion.div>
+    <motion.div
+      className="home__splash"
+    >
+      <div className="home__splash-logo">
+        <img className="home__splash-logo-icon" src={architectIcon} alt="Architect" />
+        <div className="home__splash-logo-text">Architect</div>
+        <Version />
+      </div>
+      <motion.div
+        className="home__splash-tag"
+      >
+        A tool for creating Network Canvas interviews
+      </motion.div>
+    </motion.div>
+
+    <motion.div className="home__project">
+      <img className="home__project-logo" src={networkCanvasLogo} alt="Network Canvas" />
+    </motion.div>
+  </motion.div>
+);
+
+export default Splash;
