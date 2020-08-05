2987933606ba86f48608a67516bed3c56a2bfa26
Joshua Melville <joshua.melville@northwestern.edu>
2020-08-05 11:19:20 +0100

    update welcome text

--- src/components/Home/WelcomeHeader.js
+++ src/components/Home/WelcomeHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { motion } from 'framer-motion';
+import { Button } from '@codaco/ui';
 import networkCanvasLogo from '@app/images/NC-Mark.svg';
 import headerGraphic from '@app/images/home/header-icon.svg';
 import videoPlaceholder from '@app/images/home/video-placeholder.svg';
@@ -11,8 +12,9 @@ import Group from './Group';
 import Switch from './Switch';
 import useAppState from './useAppState';
 
+
 const WelcomeHeader = () => {
-  const [isOpen, setIsOpen] = useAppState('showWelcome', false);
+  const [isOpen, setIsOpen] = useAppState('showWelcome', true);
 
   const classes = cx(
     'welcome-header',
@@ -46,7 +48,7 @@ const WelcomeHeader = () => {
       </Group>
       <motion.div
         className="welcome-header__panel"
-        initial={{ height: 'auto' }}
+        initial={{ height: 0 }}
         animate={{ height: isOpen ? 'auto' : 0 }}
       >
         <Group
@@ -63,10 +65,21 @@ const WelcomeHeader = () => {
             </div>
           </div>
           <div className="home-welcome__content">
-            <h2>Welcome</h2>
-            <p>Welcome to Network Canvas Architect! To get started, use the buttons above to
-              create a new interview protocol, or open an existing one from elsewhere. When you
-              return to this screen later, recent protocols you have opened will be shown here.
+            <h2>Welcome to Architect!</h2>
+            <p>
+              If you are new to the software, please consider watching the overview
+              video to the left. It will explain how the software works, and introduce
+              all the essential skills needed to build an interview protocol. We also
+              have extensive tutorials and information on a range of topics on our
+              documentation website, which you can visit using the link below.
+            </p>
+            <Button>Visit Documentation Website</Button>
+            <p>
+              Alternatively, to get started right away use the buttons below to
+              create a new interview protocol, or open an existing one from elsewhere
+            </p>
+            <p>
+              Thank you for supporting the Network Canvas project!
             </p>
           </div>
         </Group>
