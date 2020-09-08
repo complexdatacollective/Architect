65ffa3714e9ceac9f70f983c94d49c13cd9dfff8
Steve Mckellar <steve@wwqrd.uk>
2020-09-08 14:00:55 +0100

    Support two modes of onChange

--- src/components/Form/Fields/Select.js
+++ src/components/Form/Fields/Select.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { motion, AnimatePresence } from 'framer-motion';
-import { get } from 'lodash';
+import { get, noop } from 'lodash';
 import cx from 'classnames';
 import Icon from '@codaco/ui/lib/components/Icon';
 import Text from '@codaco/ui/lib/components/Fields/Text';
@@ -20,10 +20,12 @@ const Select = ({
   label,
   createNewOption,
   meta,
-  ...rest,
+  ...props,
 }) => {
-  const { value, onChange } = input;
+  const { value } = input;
   const { invalid, error, touched } = meta;
+
+  const onChange = input.onChange || props.onChange;
 
   const [state, setState] = useState({
     ...initialState,
@@ -45,7 +47,9 @@ const Select = ({
 
   const handleSelect = (e) => {
     const index = e.target.value;
-    const updatedValue = get(options, [index, 'value'], null);
+    // Redux form value handler needs the actual value
+    const valuePath = input.onChange ? [index, 'value'] : index;
+    const updatedValue = get(options, valuePath, null);
     setState(s => ({ ...s, isNew: false }));
     onChange(updatedValue);
   };
@@ -54,21 +58,6 @@ const Select = ({
     const newValue = e.target.value;
     setState(s => ({ ...s, newValue }));
   };
-
-  // useEffect(() => {
-  //   if (state.value !== input.value) { return; }
-  //   setState(s => ({ ...s, value }));
-  // }, [input.value]);
-
-  // useEffect(() => {
-  //   if (state.value !== input.value) { return; }
-
-  //   if (onChange) {
-  //     onChange(state.value);
-  //     console.log(input, state);
-  //   }
-  //   console.log(input, state);
-  // }, [state.value]);
 
   return (
     <motion.div className={classes}>
@@ -117,13 +106,25 @@ const Select = ({
 Select.propTypes = {
   options: PropTypes.array,
   className: PropTypes.string,
+  input: PropTypes.shape({
+    onChange: PropTypes.func,
+  }),
+  meta: PropTypes.shape({
+    invalid: PropTypes.bool,
+    error: PropTypes.string,
+    touched: PropTypes.bool,
+  }),
 };
 
 Select.defaultProps = {
   className: null,
   options: [],
-  meta: {},
-  input: {},
+  meta: {
+    invalid: false,
+    touched: false,
+    error: null,
+  },
+  input: { onChange: noop },
 };
 
 
