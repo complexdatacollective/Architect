adbf9704f6de675058c64434cf2c04d45cd15633
Steve Mckellar <steve@wwqrd.uk>
2020-09-08 13:44:35 +0100

    Select with basic interactions

--- src/components/Form/Fields/Select.js
+++ src/components/Form/Fields/Select.js
@@ -1,78 +1,127 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+import { get } from 'lodash';
 import cx from 'classnames';
 import Icon from '@codaco/ui/lib/components/Icon';
 import Text from '@codaco/ui/lib/components/Fields/Text';
 
-const getValue = (options, value) => {
-  const foundValue = options.find(option => option.value === value);
-  if (!foundValue) { return null; }
-
-  return foundValue;
+const initialState = {
+  isNew: false,
+  isNewSaved: false,
+  newValue: null,
+  value: null,
 };
-
 
 const Select = ({
   className,
-  input: { onBlur, ...input },
+  input,
   options,
   label,
   createNewOption,
-  meta: { invalid, error, touched },
+  meta,
   ...rest,
 }) => {
-  // const {
-  //   className,
-  //   input: { onBlur, ...input },
-  //   options,
-  //   children,
-  //   selectOptionComponent,
-  //   label,
-  //   createNewOption,
-  //   meta: { invalid, error, touched },
-  //   ...rest
-  // } = this.props;
+  const { value, onChange } = input;
+  const { invalid, error, touched } = meta;
 
-  // const componentClasses = cx(
-  //   className,
-  //   'form-fields-select',
-  //   {
-  //     'form-fields-select--has-error': invalid && touched && error,
-  //   },
-  // );
+  const [state, setState] = useState({
+    ...initialState,
+  });
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    console.log(value);
+  const selected = options.findIndex(option => option.value === value);
+
+  const classes = cx(
+    className,
+    'chooser',
+    {
+      'chooser--has-error': invalid && touched && error,
+    },
+  );
+
+  const handleClickCreateNew = () => {
+    setState(s => ({ ...s, isNew: true }));
   };
 
+  const handleSelect = (e) => {
+    const index = e.target.value;
+    const updatedValue = get(options, [index, 'value'], null);
+    setState(s => ({ ...s, isNew: false }));
+    onChange(updatedValue);
+  };
+
+  const handleChangeNew = (e) => {
+    const newValue = e.target.value;
+    setState(s => ({ ...s, newValue }));
+  };
+
+  // useEffect(() => {
+  //   if (state.value !== input.value) { return; }
+  //   setState(s => ({ ...s, value }));
+  // }, [input.value]);
+
+  // useEffect(() => {
+  //   if (state.value !== input.value) { return; }
+
+  //   if (onChange) {
+  //     onChange(state.value);
+  //     console.log(input, state);
+  //   }
+  //   console.log(input, state);
+  // }, [state.value]);
+
   return (
-    <motion.div>
-      <motion.div>
-        <select className>
-          <option>&mdash; Select an option &mdash;</option>
-          {options.map(({ value, label }) => (
-            <option value={value}>{label}</option>
-          ))}
-        </select>
-        { createNewOption &&
-          <button>Create new</button>
+    <motion.div className={classes}>
+      <AnimatePresence>
+        <motion.div key="options">
+          <select onChange={handleSelect} value={selected}>
+            <option>&mdash; Select an option &mdash;</option>
+            {options.map((option, index) => (
+              <option
+                value={index}
+                key={`option_${index}`}
+              >{option.label || option.value}</option>
+            ))}
+          </select>
+          { createNewOption &&
+            <button
+              onClick={handleClickCreateNew}
+            >Create new</button>
+          }
+        </motion.div>
+        { state.isNew &&
+          <motion.div
+            key="new"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <input
+              type="text"
+              value={state.newValue}
+              onChange={handleChangeNew}
+              disabled={state.isNewSaved}
+            />
+            { !state.isNewSaved && <button>Save</button> }
+            { state.isNewSaved && <button>Delete</button> }
+          </motion.div>
         }
-      </motion.div>
-      <motion.div>
-        <Text />
-        <button>Delete</button>
-      </motion.div>
-      {invalid && touched && <div className="form-fields-select__error"><Icon name="warning" />{error}</div>}
+        <motion.div key="error">
+          {invalid && touched && <div className="form-fields-select__error"><Icon name="warning" />{error}</div>}
+        </motion.div>
+      </AnimatePresence>
     </motion.div>
   );
 };
 
 Select.propTypes = {
+  options: PropTypes.array,
+  className: PropTypes.string,
 };
 
 Select.defaultProps = {
+  className: null,
+  options: [],
   meta: {},
   input: {},
 };
