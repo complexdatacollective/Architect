173f8d79a07d0c93f72579256fcc509c80f61787
Steve Mckellar <steve@wwqrd.uk>
2020-09-10 17:17:14 +0100

    Integrate creatable validation

--- src/components/Form/Fields/CreatableSelect.js
+++ src/components/Form/Fields/CreatableSelect.js
@@ -4,13 +4,43 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { get, noop } from 'lodash';
 import cx from 'classnames';
 import Icon from '@codaco/ui/lib/components/Icon';
-import Text from '@codaco/ui/lib/components/Fields/Text';
+import { getValidator } from '@app/utils/validations';
+
+const makeGetValidationErrors = (validation = {}, reserved = []) =>
+  (options, option) => {
+    const validator = getValidator(validation);
+    const validationErrors = validator(option);
+
+    // True if option contains only spaces or no chars
+    const isEmpty = option.replace(/ /g, '').length === 0;
+
+    // True if option matches the label prop of the supplied object
+    const matchLabel = ({ label: variableLabel }) =>
+      variableLabel && option &&
+      variableLabel.toLowerCase() === option.toLowerCase();
+    const alreadyExists = options.some(matchLabel);
+    const isReserved = reserved.some(matchLabel);
+
+    let warnings = null;
+
+    if (!alreadyExists && isReserved) {
+      warnings = `"${option}" is already used elsewhere in this entity`;
+    } else if (!isEmpty && validationErrors) {
+      warnings = validationErrors;
+    }
+
+    const isValid = !isEmpty && !validationErrors && !alreadyExists && !isReserved;
+
+    return [isValid, warnings];
+  };
 
 const initialState = {
   isNew: false,
   isNewSaved: false,
-  newValue: null,
+  newValue: '',
   value: null,
+  isNewValid: false,
+  newWarnings: null,
 };
 
 const Select = ({
@@ -22,12 +52,15 @@ const Select = ({
   onDeleteOption,
   disabled,
   meta,
-  ...props,
+  validation,
+  reserved,
+  ...props
 }) => {
   const { value } = input;
   const { invalid, error, touched } = meta;
 
   const onChange = input.onChange || props.onChange;
+  const getValidationErrors = makeGetValidationErrors(validation, reserved);
 
   const [state, setState] = useState({
     ...initialState,
@@ -44,13 +77,15 @@ const Select = ({
   );
 
   const handleSaveNew = () => {
+    if (!state.isNewValid) { return; }
     const result = onCreateOption(state.newValue);
     setState(s => ({ ...s, isNewSaved: true }));
     onChange(result);
   };
 
   const handleChooseCreateNew = () => {
-    setState(s => ({ ...s, isNew: true, newValue: '' }));
+    onChange(null);
+    setState(s => ({ ...s, isNew: true, newValue: '', isNewValid: false, newWarnings: null }));
   };
 
   const handleDeleteNew = () => {
@@ -70,11 +105,12 @@ const Select = ({
 
   const handleChangeNew = (e) => {
     const newValue = e.target.value;
-    setState(s => ({ ...s, newValue }));
+    const [isNewValid, newWarnings] = getValidationErrors(options, newValue);
+    setState(s => ({ ...s, newValue, isNewValid, newWarnings }));
   };
 
   const handleCancelNew = () => {
-    setState(s => ({ ...s, isNew: false }));
+    setState(s => ({ ...s, isNew: false, isNewValid: false, newWarnings: null }));
   };
 
   const selectDisabled = disabled || state.isNew;
@@ -121,46 +157,51 @@ const Select = ({
             />
             { !state.isNewSaved &&
               <Fragment>
-                <button type="button" onClick={handleSaveNew}>Save</button>
+                <button type="button" onClick={handleSaveNew} disabled={!state.isNewValid}>Save</button>
                 <button type="button" onClick={handleCancelNew}>Cancel</button>
               </Fragment>
             }
           </motion.div>
         }
-        <motion.div key="error">
-          {invalid && touched && <div className="form-fields-select__error"><Icon name="warning" />{error}</div>}
-        </motion.div>
+        { state.newWarnings &&
+          <motion.div key="warnings">
+            <div className="form-fields-select__error"><Icon name="warning" />{state.newWarnings}</div>
+          </motion.div>
+        }
+        { invalid && touched &&
+          <motion.div key="error">
+            <div className="form-fields-select__error"><Icon name="warning" />{error}</div>
+          </motion.div>
+        }
       </AnimatePresence>
     </motion.div>
   );
 };
 
 Select.propTypes = {
-  options: PropTypes.array,
   className: PropTypes.string,
-  input: PropTypes.shape({
-    onChange: PropTypes.func,
-    value: PropTypes.any,
-  }),
-  meta: PropTypes.shape({
-    invalid: PropTypes.bool,
-    error: PropTypes.string,
-    touched: PropTypes.bool,
-  }),
+  disabled: PropTypes.bool,
+  input: PropTypes
+    .shape({ onChange: PropTypes.func, value: PropTypes.any }),
+  label: PropTypes.string,
+  meta: PropTypes
+    .shape({ invalid: PropTypes.bool, error: PropTypes.string, touched: PropTypes.bool }),
+  onChange: PropTypes.func,
+  onCreateOption: PropTypes.func,
+  onDeleteOption: PropTypes.funx,
+  options: PropTypes.array,
 };
 
 Select.defaultProps = {
   className: null,
+  disabled: false,
+  input: { value: null, onChange: noop },
+  label: null,
+  meta: { invalid: false, touched: false, error: null },
+  onChange: noop,
+  onCreateOption: noop,
+  onDeleteOption: noop,
   options: [],
-  meta: {
-    invalid: false,
-    touched: false,
-    error: null,
-  },
-  input: {
-    value: null,
-    onChange: noop,
-  },
 };
 
 
