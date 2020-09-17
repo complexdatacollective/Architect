d6d5d821eb9ac24829664f9ae50d2bb181eee0aa
Steve Mckellar <steve@wwqrd.uk>
2020-09-17 17:33:48 +0100

    use yauzl

--- src/other/protocols/lib/archive.js
+++ src/other/protocols/lib/archive.js
@@ -2,6 +2,8 @@ import log from 'electron-log';
 // import fs from 'fs-extra';
 import archiver from 'archiver';
 import extractZip from 'extract-zip';
+import miss from 'mississippi';
+import yauzl from 'yauzl';
 import { createWriteStream } from 'fs';
 
 // Since this will be compressed over the wire, we choose uncompressed for speed
@@ -16,8 +18,31 @@ const archiveOptions = {
  * @param {string} destinationPath
  * @return Returns a promise that resolves to the destination path
  */
+// const extract = async (sourcePath, destinationPath) =>
+//   extractZip(sourcePath, { dir: destinationPath });
+
 const extract = async (sourcePath, destinationPath) =>
-  extractZip(sourcePath, { dir: destinationPath });
+  yauzl.open(sourcePath, { lazyEntries: true }, (err, zipfile) => {
+    if (err) throw err;
+    zipfile.readEntry();
+    zipfile.on('entry', (entry) => {
+      if (/\/$/.test(entry.fileName)) {
+        // Directory file names end with '/'.
+        // Note that entires for directories themselves are optional.
+        // An entry's fileName implicitly requires its parent directories to exist.
+        zipfile.readEntry();
+      } else {
+        // file entry
+        zipfile.openReadStream(entry, (err2, readStream) => {
+          if (err2) throw err2;
+          readStream.on('end', () => {
+            zipfile.readEntry();
+          });
+          readStream.pipe(miss.concat((data) => { console.log(data.toString()); }));
+        });
+      }
+    });
+  });
 
 /**
  * Write a bundled (zip) protocol from sourcePath (working directory) to destinationPath
