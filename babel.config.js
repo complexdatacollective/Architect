d0cd0b2249ea0246a34fc48694923b1ccecc0c60
Joshua Melville <joshua.melville@northwestern.edu>
2020-09-30 13:23:17 +0100

    update UI and framer-motion

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/react',
+  ],
+  plugins: [
+    '@babel/plugin-syntax-import-meta',
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    '@babel/plugin-proposal-json-strings',
+  ],
+};
