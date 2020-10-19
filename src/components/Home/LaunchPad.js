9f5401dcc017332d04810c6fcca32743aff938ca
Joshua Melville <joshua.melville@northwestern.edu>
2020-10-19 16:15:45 +0100

    update timeline preview images - fixes #643

--- src/components/Home/LaunchPad.js
+++ src/components/Home/LaunchPad.js
@@ -45,7 +45,7 @@ const LaunchPad = ({
             <h2>Resume Editing</h2>
             <ProtocolCard
               description={lastEditedProtocol.filePath}
-              lastModified={lastEditedProtocol.lastModified.toString()}
+              lastModified={lastEditedProtocol.lastModified}
               name={lastEditedProtocol.name}
               onClickHandler={() => handleLoadProtocol(lastEditedProtocol.filePath)}
               schemaVersion={lastEditedProtocol.schemaVersion}
@@ -58,7 +58,7 @@ const LaunchPad = ({
                   key={protocol.filePath}
                   condensed
                   description={protocol.filePath}
-                  lastModified={protocol.lastModified.toString()}
+                  lastModified={protocol.lastModified}
                   name={protocol.name}
                   onClickHandler={() => handleLoadProtocol(protocol.filePath)}
                   schemaVersion={protocol.schemaVersion}
