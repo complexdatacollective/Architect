c270bd476e97be82ae68ad0b3bc3fc05a70e70d5
Steve Mckellar <steve@wwqrd.uk>
2020-10-27 14:25:40 +0000

    fix <Stage> and <UnorderedList> prop warnings

--- src/components/Timeline/__tests__/Stage.test.js
+++ src/components/Timeline/__tests__/Stage.test.js
@@ -1,12 +1,15 @@
 /* eslint-env jest */
 
 import React from 'react';
+import uuid from 'uuid';
 import { shallow } from 'enzyme';
 import { Stage } from '../Stage';
 
 jest.mock('@codaco/ui/lib/utils/CSSVariables');
 
 const mockProps = {
+  id: uuid(),
+  stageNumber: 1,
   type: 'Foo',
   onEditStage: () => {},
   onDeleteStage: () => {},
