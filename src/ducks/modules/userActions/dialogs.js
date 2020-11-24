834a41e52c074287755d89ffd7eef7f48e34ba33
Joshua Melville <joshua.melville@northwestern.edu>
2020-11-24 18:10:54 +0000

    update error messages

--- src/ducks/modules/userActions/dialogs.js
+++ src/ducks/modules/userActions/dialogs.js
@@ -17,29 +17,96 @@ import { actionCreators as dialogActions } from '@modules/dialogs';
 const getFriendlyMessage = (e) => {
   switch (e) {
     case netcanvasFileErrors.CreateTemplateFailed:
-      return (<p>New protocol could not be created</p>);
+      return (
+        <p>
+          Architect failed to create a new protocol from the template file. This may mean
+          it could not write to the temporary directory, or it could not read the template
+          file. Please contact the Network Canvas team for support with this issue.
+        </p>
+      );
     case netcanvasFileErrors.MissingSchemaVersion:
-      return (<p>Schema version not defined in protocol</p>);
+      return (
+        <p>
+          The protocol file is invalid because it does not contain a schema version.
+          For support, please contact the Network Canvas team and provide a copy of your
+          protocol file.
+        </p>
+      );
     case netcanvasFileErrors.MissingPermissions:
-      return (<p>Netcanvas file does not have write permissions, changes will not be savable, so file was not opened</p>);
+      return (
+        <p>
+          Architect does not have permission to write to this protocol file. Write permission
+          is needed for saving changes, so the protocol could not be opened. Please change
+          the permissions on this file, and try again.
+        </p>
+      );
     case netcanvasFileErrors.ExtractFailed:
-      return (<p>Netcanvas file could not be opened, it may be corrupted</p>);
+      return (
+        <p>
+          An error ocurred extracting the protocol file, and it may be corrupted. If you downloaded
+          the file or copied it from another device, try repeating the process. For support, email
+          the Network Canvas team with a copy of your protocol file.
+        </p>
+      );
     case netcanvasFileErrors.BackupFailed:
-      return (<p>Netcanvas backup could not be created, save aborted, original file unchanged</p>);
+      return (
+        <p>
+          Opening failed because a backup file could not be created. Check that you have enough disk
+          space, and that your protocol file is not in a read-only location. Your protocol file has
+          not been modified.
+        </p>);
     case netcanvasFileErrors.SaveFailed:
-      return (<p>Protocol data could not be saved</p>);
+      return (
+        <p>
+          Saving failed because the protocol data could not be written. Check that you have enough
+          disk space, or try saving to a different location. If you continue to encounter this
+          error, please contact the Network Canvas team for support.
+        </p>);
     case netcanvasFileErrors.ArchiveFailed:
-      return (<p>Netcanvas file could not be created</p>); // same as SaveFailed?
+      return (
+        <p>
+          Saving failed because the protocol data could not be compressed. Check that you
+          have enough disk space, or try saving to a different location. If you continue to
+          encounter this error, please contact the Network Canvas team for support.
+        </p>
+      ); // same as SaveFailed?
     case netcanvasFileErrors.DeployFailed:
-      return (<p>Deployment failed, final file could not be moved</p>);
+      return (
+        <p>
+          Saving failed, because the working copy could not be moved to the location you
+          specified. Try saving the protocol to a different location. If you continue to
+          encounter this error, please contact the Network Canvas team for support.
+        </p>
+      );
     case netcanvasFileErrors.MissingProtocolJson:
-      return (<p>Protocol does not have a json file</p>); // TODO not suitable for user?
+      return (
+        <p>
+          This protocol file is corrupt or invalid. For support, please email
+          the Network Canvas team with a copy of your protocol file.
+        </p>
+      ); // TODO not suitable for user?
     case netcanvasFileErrors.ProtocolJsonParseError:
-      return (<p>Protocol json could not be parsed</p>);
+      return (
+        <p>
+          This protocol file is corrupt or invalid. For support, please email
+          the Network Canvas team with a copy of your protocol file.
+        </p>
+      );
     case netcanvasFileErrors.NetcanvasCouldNotValidate:
-      return (<p>Netcanvas file could not be validated</p>);
+      return (
+        <p>
+          This protocol file failed validation. For support, please email
+          the Network Canvas team with a copy of your protocol file.
+        </p>
+      );
     case netcanvasFileErrors.NetcanvasVerificationError:
-      return (<p>Netcanvas file could not be verifed</p>);
+      return (
+        <p>
+          The process failed because the file written did not match what was
+          expected. For support, please email the Network Canvas team with
+          a copy of your protocol file.
+        </p>
+      );
     default:
       return null;
   }
@@ -66,7 +133,7 @@ const validationErrorDialog = (e) => {
       </p>
       <p>
         You may still save and edit the protocol but it <strong>
-          will not be compatable with
+          will not be compatible with
           Network Canvas or Server</strong>.
       </p>
     </React.Fragment>
