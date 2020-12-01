9e5f1168affb84e465e949eae48d4d6e0edd7097
Steve Mckellar <steve@wwqrd.uk>
2020-12-01 11:54:40 +0000

    Stub userAction tests

---
+++ src/ducks/modules/userActions/__tests__/userActions.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+
+import { actionCreators, actionLocks } from '../userActions';
+
+describe('userActions', () => {
+  it('exports actionLocks', () => {
+    expect(actionLocks).toEqual(
+      expect.objectContaining({
+        loading: expect.any(Function),
+        protocols: expect.any(Function),
+        saving: expect.any(Function),
+      }),
+    );
+  });
+
+  describe('actions', () => {
+    it.todo('openNetcanvas');
+    it.todo('createNetcanvas');
+    it.todo('saveAsNetcanvas');
+    it.todo('saveNetcanvas');
+  });
+});
