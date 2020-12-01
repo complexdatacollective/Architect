cc7d28e231868a5a2090d95179fa11ff34fb4d5e
Steve Mckellar <steve@wwqrd.uk>
2020-12-01 13:01:50 +0000

    Test userActions

--- src/ducks/modules/userActions/__tests__/userActions.test.js
+++ src/ducks/modules/userActions/__tests__/userActions.test.js
@@ -1,6 +1,39 @@
 /* eslint-env jest */
 
+import { getHasUnsavedChanges } from '@selectors/session';
+import { actionCreators as dialogsActions } from '@modules/dialogs';
+import {
+  openDialog,
+} from '@app/utils/dialogs';
+import {
+  checkSchemaVersion,
+  validateNetcanvas,
+} from '@app/utils/netcanvasFile';
+
+import { actionCreators as sessionActions } from '@modules/session';
 import { actionCreators, actionLocks } from '../userActions';
+
+const { schemaVersionStates } = jest.requireActual('@app/utils/netcanvasFile');
+
+jest.mock('@app/utils/dialogs');
+jest.mock('@modules/dialogs');
+jest.mock('@modules/session');
+jest.mock('@selectors/session');
+jest.mock('@app/utils/netcanvasFile');
+
+const getState = jest.fn(() => ({
+  ui: {
+    status: { busy: [] },
+    screens: {},
+  },
+}));
+
+const dispatch = jest.fn((arg) => {
+  if (typeof arg === 'function') {
+    return arg(dispatch, getState);
+  }
+  return arg;
+});
 
 describe('userActions', () => {
   it('exports actionLocks', () => {
@@ -14,7 +47,15 @@ describe('userActions', () => {
   });
 
   describe('actions', () => {
-    it.todo('openNetcanvas');
+    it('openNetcanvas', () => {
+      getHasUnsavedChanges.mockResolvedValueOnce(false);
+      checkSchemaVersion.mockResolvedValueOnce(['X', schemaVersionStates.OK]);
+      validateNetcanvas.mockResolvedValueOnce(true);
+      sessionActions.openNetcanvas.mockResolvedValueOnce(true);
+      dispatch(actionCreators.openNetcanvas());
+      console.log(dispatch.mock.calls);
+      expect(dispatch.mock.calls).toBe(null);
+    });
     it.todo('createNetcanvas');
     it.todo('saveAsNetcanvas');
     it.todo('saveNetcanvas');
--- src/utils/__mocks__/dialogs.js
+++ src/utils/__mocks__/dialogs.js
@@ -1,9 +1,11 @@
 /* eslint-env jest */
 
-const openProtocolDialog = jest.fn(() => Promise.resolve('/dev/null/fake/user/explored/path'));
-const saveProtocolDialog = jest.fn(() => Promise.resolve('/dev/null/fake/user/save/path'));
+// const openProtocolDialog = jest.fn(() => Promise.resolve('/dev/null/fake/user/explored/path'));
+// const saveProtocolDialog = jest.fn(() => Promise.resolve('/dev/null/fake/user/save/path'));
+const openDialog = jest.fn(() => Promise.resolve({ canceled: false, filePaths: ['/dev/null/fake/user/explored/path'] }));
 
 export {
-  openProtocolDialog,
-  saveProtocolDialog,
+  // openProtocolDialog,
+  // saveProtocolDialog,
+  openDialog,
 };
