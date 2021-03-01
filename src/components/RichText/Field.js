f7c6ce68d3c6970ca9824684577963a162473c09
Steve Mckellar <steve@wwqrd.uk>
2021-03-01 17:34:04 +0000

    Move to storybook in UI to speed up dev

--- src/components/RichText/Field.js
+++ src/components/RichText/Field.js
@@ -1,38 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import slate, { serialize as serializeMarkdown } from 'remark-slate';
+import { Node, Text } from 'slate';
+import unified from 'unified';
+import markdown from 'remark-parse';
+
 import RichText from './RichText';
 
+const initialValue = [{
+  children: [
+    { text: '' },
+  ],
+}];
+
+const parse = (value) => {
+  const data = unified()
+    .use(markdown)
+    .use(slate)
+    .parse(value);
+  console.log({ value, data });
+
+  return [data];
+};
+
+const serialize = (node) => {
+  return '';
+  // console.log({ node });
+
+  // if (Text.isText(node)) {
+  //   // return escapeHtml(node.text)
+  //   return node.text;
+  // }
+
+  // return node.map(n => serializeMarkdown(n)).join('\n');
+};
+
 const getValue = (value) => {
-  try {
-    return JSON.parse(value);
-  } catch (e) {
-    return [{
-      type: 'paragraph',
-      children: [
-        { text: 'This is editable ' },
-        { text: 'rich', bold: true },
-        { text: ' text, ' },
-        { text: 'much', italic: true },
-        { text: ' better than a ' },
-        { text: '<textarea>', code: true },
-        { text: '!' },
-      ],
-    }];
+  if (value && value !== '') {
+    // TODO: parse value
+    return parse(value);
   }
+
+  return initialValue;
 };
 
 const RichTextField = ({ input, ...props }) => {
-
   const [value, setValue] = useState(getValue(input.value));
 
   const handleChange = (newValue) => {
     setValue(newValue);
   };
 
-  const newValueString = JSON.stringify(value);
+  const markdownValue = serialize(value);
 
   useEffect(() => {
-    input.onChange(newValueString);
-  }, [newValueString]);
+    input.onChange(markdownValue);
+  }, [input.onChange, markdownValue]);
 
   return <RichText value={value} onChange={setValue} />;
 };
