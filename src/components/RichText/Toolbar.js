fad02d7b457ba1e87493b855827e54f148c1f5a1
Steve Mckellar <steve@wwqrd.uk>
2021-03-02 16:12:24 +0000

    Add icons to RichText

--- src/components/RichText/Toolbar.js
+++ src/components/RichText/Toolbar.js
@@ -9,7 +9,7 @@ const Toolbar = ({ controls }) => (
   <div className="rich-text__toolbar">
     { includes(controls, 'bold') && <MarkButton format="bold" icon="bold" /> }
     { includes(controls, 'italic') && <MarkButton format="italic" icon="italic" /> }
-    { includes(controls, 'underline') && <MarkButton format="underline" icon="underlined" /> }
+    { includes(controls, 'underline') && <MarkButton format="underline" icon="underline" /> }
     { includes(controls, 'code') && <MarkButton format="code" icon="code" /> }
     { includes(controls, 'headings') && (
       <Fragment>
