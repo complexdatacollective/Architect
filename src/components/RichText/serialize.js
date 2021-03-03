1603c2418003cc1d023642893a5186db7dd71b5c
Steve Mckellar <steve@wwqrd.uk>
2021-03-03 17:40:47 +0000

    Looks like double \n is needed to correctly reparse?

--- src/components/RichText/serialize.js
+++ src/components/RichText/serialize.js
@@ -1,6 +1,6 @@
 import { serialize } from 'remark-slate';
 
 const serializeNodes = nodes =>
-  nodes.map(n => serialize(n)).join('\n\n');
+  nodes.map(n => serialize(n)).join('\n');
 
 export default serializeNodes;
