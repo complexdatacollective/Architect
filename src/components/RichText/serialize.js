24828ed03d69be6b94acd2095af924c53c1ced77
Steve Mckellar <steve@wwqrd.uk>
2021-03-03 17:32:55 +0000

    Double newline between blocks

--- src/components/RichText/serialize.js
+++ src/components/RichText/serialize.js
@@ -1,6 +1,6 @@
 import { serialize } from 'remark-slate';
 
 const serializeNodes = nodes =>
-  nodes.map(n => serialize(n)).join('');
+  nodes.map(n => serialize(n)).join('\n\n');
 
 export default serializeNodes;
