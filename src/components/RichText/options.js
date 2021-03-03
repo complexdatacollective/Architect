e6946951dde7fe42e87b7f4a6ac6260f60413db8
Steve Mckellar <steve@wwqrd.uk>
2021-03-03 15:50:29 +0000

    use 'modes' for richtext options

---
+++ src/components/RichText/options.js
@@ -0,0 +1,30 @@
+export const BLOCKS = {
+  quote: 'block_quote',
+  headings: [
+    'heading_one',
+    'heading_two',
+    'heading_three',
+    'heading_four',
+    'heading_five',
+  ],
+  lists: ['ul_list', 'ol_list'],
+};
+
+export const MARKS = {
+  bold: 'bold',
+  italic: 'italic',
+  underline: 'underline',
+  code: 'code',
+};
+
+export const MODES = {
+  full: 'full',
+  marks: 'marks',
+  single: 'single',
+};
+
+export const TOOLBAR_MODES = {
+  full: [...Object.keys(BLOCKS), ...Object.keys(MARKS)],
+  marks: Object.keys(MARKS),
+  single: Object.keys(MARKS),
+};
