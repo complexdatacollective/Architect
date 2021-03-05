822e3f27c32d637dae2e8d7d7cc9db5573914c16
Steve Mckellar <steve@wwqrd.uk>
2021-03-05 10:27:49 +0000

    Document withNormalize

--- src/components/RichText/withNormalize.js
+++ src/components/RichText/withNormalize.js
@@ -6,36 +6,53 @@ const defaultOptions = {
   mode: MODES.full,
 };
 
+/**
+ * This extends the editor with a custom normalization
+ * function to support 'single' line, and 'marks' only
+ * modes.
+ */
 const withNormalize = (editor, userOptions) => {
   const { normalizeNode } = editor;
   const options = { ...defaultOptions, ...userOptions };
 
   editor.normalizeNode = ([node, path]) => {
-    // for top level paths only
-    if (path.length === 0) {
-      if (options.mode === MODES.single) {
-        // If empty, insert a blank paragraph node
-        if (editor.children.length < 1) {
-          const defaultNode = { type: 'paragraph', children: [{ text: '' }] };
-          Transforms.insertNodes(editor, defaultNode, { at: path.concat(0) });
-        }
+    /**
+     * 'single' mode
+     *
+     * We can the top-level nodes, for each
+     * subsequent element after the first we
+     * merge it with the previous, creating
+     * a single node.
+     */
 
-        // Force the first node to always be a paragraph and merge any
-        // later nodes
-        for (const [child, childPath] of Node.children(editor, path)) {
-          if (Element.isElement(child) && childPath[0] === 0 && node.type !== 'paragraph') {
-            Transforms.setNodes(
-              editor,
-              { type: 'paragraph', break: false },
-              { at: childPath },
-            );
-          } else if (Element.isElement(child)) {
-            Transforms.mergeNodes(editor, { at: childPath });
-          }
+    // for top level paths only
+    if (options.mode === MODES.single && path.length === 0) {
+      // If empty, insert a blank paragraph node
+      if (editor.children.length < 1) {
+        const defaultNode = { type: 'paragraph', children: [{ text: '' }] };
+        Transforms.insertNodes(editor, defaultNode, { at: path.concat(0) });
+      }
+
+      // Force the first node to always be a paragraph and merge any
+      // later nodes
+      for (const [child, childPath] of Node.children(editor, path)) {
+        if (Element.isElement(child) && childPath[0] === 0 && node.type !== 'paragraph') {
+          Transforms.setNodes(
+            editor,
+            { type: 'paragraph', break: false },
+            { at: childPath },
+          );
+        } else if (Element.isElement(child)) {
+          Transforms.mergeNodes(editor, { at: childPath });
         }
       }
     }
 
+    /**
+     * 'marks' mode
+     *
+     * Ensures that all elements are paragraphs
+     */
     // Filter allowed Elements
     if (options.mode === MODES.marks) {
       if (Element.isElement(node) && node.type !== 'paragraph') {
