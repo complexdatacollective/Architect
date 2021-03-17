46614668dc2b5568f2dcea025875aaf44598ecd7
Steve Mckellar <steve@wwqrd.uk>
2021-03-17 16:16:11 +0000

    Remove epics pt.1

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import { createEpicMiddleware, combineEpics } from 'redux-observable';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import logger from './middleware/logger';
 import ipc from './ipc';
-import { rootReducer, rootEpic as architectRootEpic } from './modules/root';
+import { rootReducer } from './modules/root';
 
 const persistConfig = {
   key: 'architect',
@@ -21,22 +20,16 @@ const persistConfig = {
 
 const getReducer = () => persistReducer(persistConfig, rootReducer);
 
-const rootEpic = combineEpics(
-  architectRootEpic,
-);
-
-const epics = createEpicMiddleware(rootEpic);
-
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const getMiddleware = () => {
   if (process.env.TEST) {
-    return [thunk, epics];
+    return [thunk];
   }
 
-  return [thunk, logger, ipc, epics];
+  return [thunk, logger, ipc];
 };
 
 const getEnhancers = () => composeEnhancers(
