c602e0b78dceb26b805f0114c1283a60fbf52cbc
Joshua Melville <joshua.melville@northwestern.edu>
2021-03-30 22:19:32 +0200

    remove RichText component and move to UI

--- src/components/RichText/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './RichText';
-export { default as Field } from './Field';
-export { default as RichTextField } from './Field';
-export { MODES } from './options';
--- src/components/RichText/serialize.js
+++
@@ -1,7 +0,0 @@
-import { serialize } from 'remark-slate';
-
-const serializeNodes = (nodes) => (
-  nodes.map((n) => serialize(n)).join('\n')
-);
-
-export default serializeNodes;
