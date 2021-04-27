84476f810eb6d3fc2950f76b3c7c2c9211170bd9
Steve Mckellar <steve@wwqrd.uk>
2021-04-27 11:06:43 +0100

    Centralize entity path definition in usage index

--- src/selectors/indexes.js
+++ src/selectors/indexes.js
@@ -14,22 +14,61 @@ const mapAssetItems = ({ type, content }, path) => {
 };
 
 /**
+ * Locations of referenceable entities in a standard protocol.
+ *
+ * This is generally used to find which entities are used where.
+ */
+const paths = {
+  edges: [
+    'stages[].prompts[].edges.create',
+    'stages[].prompts[].edges.display[]',
+    'stages[].presets[].edges.display[]',
+    'stages[].prompts[].createEdge',
+    ['stages[].subject', mapSubject('edge')],
+  ],
+  nodes: [
+    ['stages[].subject', mapSubject('node')],
+  ],
+  variables: [
+    'stages[].quickAdd',
+    'stages[].form.fields[].variable',
+    'stages[].panels.filter.rules[].options.attribute',
+    'stages[].searchOptions.matchProperties[]',
+    'stages[].cardOptions.additionalProperties[].variable',
+    'stages[].prompts[].variable',
+    'stages[].prompts[].edgeVariable',
+    'stages[].prompts[].otherVariable',
+    'stages[].prompts[].additionalAttributes[].variable',
+    'stages[].prompts[].highlight.variable',
+    'stages[].prompts[].layout.layoutVariable',
+    'stages[].prompts[].presets[].layoutVariable',
+    'stages[].prompts[].presets[].groupVariable',
+    'stages[].prompts[].presets[].edges.display[]',
+    'stages[].prompts[].presets[].highlight[]',
+    'stages[].prompts[].bucketSortOrder[].property',
+    'stages[].prompts[].binSortOrder[].property',
+    'stages[].skipLogic.filter.rules[].options.attribute',
+    'stages[].filter.rules[].options.attribute',
+    'stages[].presets[].layoutVariable',
+    'stages[].presets[].groupVariable',
+    'stages[].presets[].edges.display[]',
+    'stages[].presets[].highlight[]',
+  ],
+  assets: [
+    'stages[].panels[].dataSource',
+    'stages[].dataSource',
+    'stages[].background.image',
+    ['stages[].items[]', mapAssetItems],
+  ],
+};
+
+/**
  * Returns index of used edges (entities)
  * @returns {object} in format: { [path]: variable }
  */
 const getEdgeIndex = createSelector(
   getProtocol,
-  (protocol) => {
-    const edgePaths = [
-      'stages[].prompts[].edges.create',
-      'stages[].prompts[].edges.display[]',
-      'stages[].presets[].edges.display[]',
-      'stages[].prompts[].createEdge',
-      ['stages[].subject', mapSubject('edge')],
-    ];
-
-    return collectPaths(edgePaths, protocol);
-  },
+  (protocol) => collectPaths(paths.edges, protocol),
 );
 
 /**
@@ -38,9 +77,7 @@ const getEdgeIndex = createSelector(
  */
 const getNodeIndex = createSelector(
   getProtocol,
-  (protocol) => collectPaths([
-    ['stages[].subject', mapSubject('node')],
-  ], protocol),
+  (protocol) => collectPaths(paths.nodes, protocol),
 );
 
 /**
@@ -49,35 +86,7 @@ const getNodeIndex = createSelector(
  */
 const getVariableIndex = createSelector(
   getProtocol,
-  (protocol) => {
-    const variablePaths = [
-      'stages[].quickAdd',
-      'stages[].form.fields[].variable',
-      'stages[].panels.filter.rules[].options.attribute',
-      'stages[].searchOptions.matchProperties[]',
-      'stages[].cardOptions.additionalProperties[].variable',
-      'stages[].prompts[].variable',
-      'stages[].prompts[].edgeVariable',
-      'stages[].prompts[].otherVariable',
-      'stages[].prompts[].additionalAttributes[].variable',
-      'stages[].prompts[].highlight.variable',
-      'stages[].prompts[].layout.layoutVariable',
-      'stages[].prompts[].presets[].layoutVariable',
-      'stages[].prompts[].presets[].groupVariable',
-      'stages[].prompts[].presets[].edges.display[]',
-      'stages[].prompts[].presets[].highlight[]',
-      'stages[].prompts[].bucketSortOrder[].property',
-      'stages[].prompts[].binSortOrder[].property',
-      'stages[].skipLogic.filter.rules[].options.attribute',
-      'stages[].filter.rules[].options.attribute',
-      'stages[].presets[].layoutVariable',
-      'stages[].presets[].groupVariable',
-      'stages[].presets[].edges.display[]',
-      'stages[].presets[].highlight[]',
-    ];
-
-    return collectPaths(variablePaths, protocol);
-  },
+  (protocol) => collectPaths(paths.variables, protocol),
 );
 
 /**
@@ -86,16 +95,7 @@ const getVariableIndex = createSelector(
  */
 const getAssetIndex = createSelector(
   getProtocol,
-  (protocol) => {
-    const assetPaths = [
-      'stages[].panels[].dataSource',
-      'stages[].dataSource',
-      'stages[].background.image',
-      ['stages[].items[]', mapAssetItems],
-    ];
-
-    return collectPaths(assetPaths, protocol);
-  },
+  (protocol) => collectPaths(paths.assets, protocol),
 );
 
 const combineLists = (lists) => lists
