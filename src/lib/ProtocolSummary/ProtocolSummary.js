935621ebced57ba4cc8541cc45a25091397dd600
Steve Mckellar <steve@wwqrd.uk>
2021-05-06 15:49:43 +0100

    Scss wip

--- src/lib/ProtocolSummary/ProtocolSummary.js
+++ src/lib/ProtocolSummary/ProtocolSummary.js
@@ -4,20 +4,30 @@ import Codebook from './components/Codebook';
 import Stages from './components/Stages';
 import SummaryContext from './components/SummaryContext';
 import { getCodebookIndex } from './helpers';
+import './ProtocolSummary.scss';
 
 const ProtocolSummary = ({ protocol }) => {
   if (!protocol) { return null; }
 
   const index = getCodebookIndex(protocol);
 
+  console.log({ protocol });
+
   return (
     <SummaryContext.Provider value={{ protocol, index }}>
       <div className="printable-summary">
-        <h1>Protocol Summary</h1>
+        <div className="printable-summary__heading">
+          <h1>Protocol Summary</h1>
+          <h2>{protocol.description}</h2>
+        </div>
 
-        <Stages />
+        <div className="printable-summary__stages">
+          <Stages />
+        </div>
 
-        <Codebook />
+        <div className="printable-summary__codebook">
+          <Codebook />
+        </div>
 
       </div>
     </SummaryContext.Provider>
