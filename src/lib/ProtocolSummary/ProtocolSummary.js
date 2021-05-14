17df2c173e9c00b0cdb70962eca8d5af44ba5948
Steve Mckellar <steve@wwqrd.uk>
2021-05-14 10:43:18 +0100

    Use window.print() and move print bar

--- src/lib/ProtocolSummary/ProtocolSummary.js
+++ src/lib/ProtocolSummary/ProtocolSummary.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { remote } from 'electron';
-import path from 'path';
-import fse from 'fs-extra';
-import os from 'os';
 import Button from '@codaco/ui/lib/components/Button';
 import Cover from './components/Cover';
 import Contents from './components/Contents';
@@ -13,23 +9,7 @@ import SummaryContext from './components/SummaryContext';
 import { getCodebookIndex } from './helpers';
 
 const print = () => {
-  console.log('print');
-  const win = remote.BrowserWindow.getFocusedWindow();
-
-  const options = {
-    printBackground: true,
-    pageSize: 'A4',
-  };
-
-  win.webContents.printToPDF(options).then((data) => {
-    const pdfPath = path.join(os.homedir(), 'Desktop', 'temp.pdf');
-    fse.writeFile(pdfPath, data, (error) => {
-      if (error) { throw error; }
-      console.log(`Wrote PDF successfully to ${pdfPath}`);
-    });
-  }).catch((error) => {
-    console.log(`Failed to write PDF to ${pdfPath}: `, error);
-  });
+  window.print();
 };
 
 const ProtocolSummary = ({ data }) => {
@@ -38,8 +18,6 @@ const ProtocolSummary = ({ data }) => {
   const { protocol, filePath } = data;
 
   const index = getCodebookIndex(protocol);
-
-  console.log({ protocol });
 
   return (
     <SummaryContext.Provider value={{ protocol, filePath, index }}>
