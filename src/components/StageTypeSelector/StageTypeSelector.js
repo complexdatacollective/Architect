9214f4487b3979806a41da954b4922be0489d6cf
Steve Mckellar <steve@wwqrd.uk>
2021-09-14 12:07:23 +0100

    Search results

--- src/components/StageTypeSelector/StageTypeSelector.js
+++ src/components/StageTypeSelector/StageTypeSelector.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import cx from 'classnames';
 import Interface, { interfaces } from './Interface';
@@ -32,87 +32,132 @@ const MenuItem = ({ category, children, selected, onClick }) => {
 };
 
 const StageTypeSelector = () => {
-  const [query, setQuery] = useState();
+  const [query, setQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState();
+
   const handleSelectCategory = useCallback((e) => {
     const { target } = e;
     const category = target.getAttribute('data-category');
     if (category === 'all') { return setSelectedCategory(); }
     setSelectedCategory(category);
-  });
+  }, [setSelectedCategory]);
 
+  const handleUpdateQuery = useCallback((e) => {
+    const { target } = e;
+    const newQuery = target.value;
+    setQuery(newQuery);
+  }, [setQuery]);
+
+  const selectableInterfaces = useMemo(
+    () => interfaces.filter(
+      ({ category }) => isSelectable(selectedCategory, category),
+    ),
+    [selectedCategory],
+  );
+
+  const resultInterfaces = useMemo(
+    () => interfaces.filter(
+      ({ name }) => name.toLowerCase().includes(query.toLowerCase()),
+    ),
+    [query],
+  );
 
   return (
     <motion.div className="stage-type-selector">
       <motion.div className="stage-type-selector__search">
-        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <input
+          type="text"
+          value={query}
+          onChange={handleUpdateQuery}
+        />
       </motion.div>
-      <motion.div className="stage-type-selector__categorized">
-        <motion.div className="stage-type-selector__menu">
-          <motion.ul className="stage-type-selector__menu-items">
-            <MenuItem
-              onClick={handleSelectCategory}
-              category="all"
-            >
-              All
-            </MenuItem>
-            <MenuItem
-              onClick={handleSelectCategory}
-              category="name-generator"
-              selected={selectedCategory === 'name-generator'}
-            >
-              Name Generators
-            </MenuItem>
-            <MenuItem
-              onClick={handleSelectCategory}
-              category="sociogram"
-              selected={selectedCategory === 'sociogram'}
-            >
-              Sociograms
-            </MenuItem>
-            <MenuItem
-              onClick={handleSelectCategory}
-              category="interpreter"
-              selected={selectedCategory === 'interpreter'}
-            >
-              Interpreters
-            </MenuItem>
-            <MenuItem
-              onClick={handleSelectCategory}
-              category="utility"
-              selected={selectedCategory === 'utility'}
-            >
-              Utilities
-            </MenuItem>
-          </motion.ul>
-          <p>Need help?</p>
-        </motion.div>
-        <motion.div className="stage-type-selector__interfaces">
-          <AnimatePresence>
-            {interfaces.map(({ id, category }) => (
-              isSelectable(selectedCategory, category) && (
+      <motion.div className="stage-type-selector__container">
+        <motion.div className="stage-type-selector__categorized">
+          <motion.div className="stage-type-selector__menu">
+            <motion.ul className="stage-type-selector__menu-items">
+              <MenuItem
+                onClick={handleSelectCategory}
+                category="all"
+              >
+                All
+              </MenuItem>
+              <MenuItem
+                onClick={handleSelectCategory}
+                category="name-generator"
+                selected={selectedCategory === 'name-generator'}
+              >
+                Name Generators
+              </MenuItem>
+              <MenuItem
+                onClick={handleSelectCategory}
+                category="sociogram"
+                selected={selectedCategory === 'sociogram'}
+              >
+                Sociograms
+              </MenuItem>
+              <MenuItem
+                onClick={handleSelectCategory}
+                category="interpreter"
+                selected={selectedCategory === 'interpreter'}
+              >
+                Interpreters
+              </MenuItem>
+              <MenuItem
+                onClick={handleSelectCategory}
+                category="utility"
+                selected={selectedCategory === 'utility'}
+              >
+                Utilities
+              </MenuItem>
+            </motion.ul>
+            <p>Need help?</p>
+          </motion.div>
+          <motion.div className="stage-type-selector__interfaces">
+            <AnimatePresence>
+              {selectableInterfaces.map(({ id }) => (
                 <motion.div
                   className="stage-type-selector__interface-container"
                   variants={animations}
                   initial="hide"
                   exit="hide"
                   animate="show"
+                  key={id}
                 >
                   <Interface id={id} />
                 </motion.div>
-              )
-            ))}
-          </AnimatePresence>
+              ))}
+            </AnimatePresence>
+          </motion.div>
         </motion.div>
-      </motion.div>
-      <motion.div className="stage-type-selector__results">
-        <motion.div className="stage-type-selector__interfaces">
-          {[1, 2, 3, 4, 5, 6].map(() => (
-            <motion.div className="stage-type-selector__interface-container">
-              <Interface />
+        <AnimatePresence>
+          { query !== '' && (
+            <motion.div
+              className="stage-type-selector__results"
+              variants={animations}
+              initial="hide"
+              exit="hide"
+              animate="show"
+              key="results"
+            >
+              <motion.div className="stage-type-selector__interfaces">
+                <AnimatePresence>
+                  {resultInterfaces.map(({ id }) => (
+                    <motion.div
+                      className="stage-type-selector__interface-container"
+                      variants={animations}
+                      initial="hide"
+                      exit="hide"
+                      animate="show"
+                      key={id}
+                    >
+                      <Interface id={id} />
+                    </motion.div>
+                  ))}
+                </AnimatePresence>
+              </motion.div>
             </motion.div>
-          ))}
-        </motion.div>
+          )}
+        </AnimatePresence>
       </motion.div>
     </motion.div>
   );
