770c73eab41b4d84c655be9cda3ad5b7cd814767
Steve Mckellar <steve@wwqrd.uk>
2021-09-22 16:38:23 +0100

    Finish animate wizard

--- src/components/Screens/GuidedNewStageScreen/Steps.js
+++ src/components/Screens/GuidedNewStageScreen/Steps.js
@@ -2,12 +2,7 @@ import React, { useState, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Button } from '@codaco/ui';
-
-const stepVariants = {
-  show: { opacity: 1, duration: 20 },
-  hide: { opacity: 0, duration: 20 },
-  exitLeft: { opacity: 0, translateX: '-100%', duration: 20 },
-};
+import { getCSSVariableAsNumber } from '@codaco/ui/lib/utils/CSSVariables';
 
 const Steps = ({
   steps,
@@ -15,6 +10,30 @@ const Steps = ({
 }) => {
   const initialState = initialStep ? [initialStep] : [steps[0].id];
   const [history, setHistory] = useState(initialState);
+
+  const stepVariants = useMemo(() => {
+    const standardDuration = getCSSVariableAsNumber('--animation-duration-standard-ms') / 1000;
+
+    return {
+      initial: { opacity: 0, x: 0 },
+      animate: {
+        opacity: 1,
+        x: 0,
+        transition: {
+          delay: standardDuration,
+          duration: standardDuration,
+        },
+      },
+      exit: {
+        opacity: 0,
+        x: 'calc(-50%)',
+        position: 'absolute',
+        y: 0,
+        width: '100%',
+        transition: { duration: standardDuration },
+      },
+    };
+  }, []);
 
   const handlePreviousStep = useCallback(() => (
     setHistory((s) => {
@@ -41,29 +60,27 @@ const Steps = ({
 
   const { actions, content } = currentStep;
 
-  console.log({ currentStepId });
-
   return (
     <motion.div className="guided-new-stage-screen__steps stage-editor-section" layout>
       <AnimatePresence>
         <motion.div
           className="guided-new-stage-screen__step"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0, translateX: '-100%', translateY: '50px', duration: 5 }}
-          layout
+          variants={stepVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
           key={currentStepId}
         >
-          <motion.div className="guided-new-stage-screen__step-content">
+          <div className="guided-new-stage-screen__step-content">
             {content}
-          </motion.div>
-          <motion.div className="guided-new-stage-screen__step-controls">
-            <motion.div className="guided-new-stage-screen__step-back">
+          </div>
+          <div className="guided-new-stage-screen__step-controls">
+            <div className="guided-new-stage-screen__step-back">
               { history.length > 1 && (
                 <Button onClick={handlePreviousStep} color="charcoal">Previous step</Button>
               )}
-            </motion.div>
-            <motion.div className="guided-new-stage-screen__step-actions">
+            </div>
+            <div className="guided-new-stage-screen__step-actions">
               { actions && actions.map(({
                 label,
                 step,
@@ -82,8 +99,8 @@ const Steps = ({
                   </Button>
                 );
               })}
-            </motion.div>
-          </motion.div>
+            </div>
+          </div>
         </motion.div>
       </AnimatePresence>
     </motion.div>
