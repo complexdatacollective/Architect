2bda0f2a001a756603e604f21dab50448aca7dcf
Steve Mckellar <steve@wwqrd.uk>
2021-09-27 16:04:05 +0100

    Remove categories from new stage menu

--- src/components/Screens/NewStageScreen/CategorizedInterfaceList.js
+++ src/components/Screens/NewStageScreen/CategorizedInterfaceList.js
@@ -1,75 +1,25 @@
 import React, { useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
-import cx from 'classnames';
 import { get } from 'lodash';
 import Tag from '@components/Tag';
 import {
   INTERFACE_TYPES,
-  CATEGORIES,
   TAGS,
   TAG_COLORS,
 } from './interfaceOptions';
 import InterfaceList from './InterfaceList';
-
-const isCategorySelected = (selectedCategory, category) => {
-  if (!selectedCategory) { return true; }
-  return category === selectedCategory;
-};
 
 const allTagsSelected = (selectedTags, tags) => {
   if (selectedTags.length === 0) { return true; }
   return selectedTags.every((tag) => tags.includes(tag));
 };
 
-const MenuItem = ({
-  category,
-  children,
-  onClick,
-  selected,
-}) => {
-  const classes = cx(
-    'new-stage-screen__menu-item',
-    {
-      'new-stage-screen__menu-item--selected': selected,
-    },
-  );
-
-  const handleClick = useCallback(() => {
-    onClick(category);
-  }, [onClick, category]);
-
-  return (
-    <li className={classes}>
-      <div onClick={handleClick}>
-        {children}
-      </div>
-    </li>
-  );
-};
-
-MenuItem.propTypes = {
-  category: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired,
-  selected: PropTypes.bool,
-};
-
-MenuItem.defaultProps = {
-  selected: false,
-};
-
 const CategorizedInterfaceList = ({
   onSelect,
   menuOther,
 }) => {
-  const [selectedCategory, setSelectedCategory] = useState();
   const [selectedTags, setSelectedTags] = useState([]);
-
-  const handleChangeCategory = useCallback((category) => {
-    if (category === 'all') { return setSelectedCategory(undefined); }
-    return setSelectedCategory(category);
-  }, [setSelectedCategory]);
 
   const handleAddTag = useCallback((newTag) => {
     setSelectedTags((tags) => [...tags, newTag]);
@@ -86,12 +36,9 @@ const CategorizedInterfaceList = ({
   const selectableInterfaces = useMemo(
     () => INTERFACE_TYPES
       .filter(
-        ({ category, tags }) => (
-          isCategorySelected(selectedCategory, category)
-          && allTagsSelected(selectedTags, tags)
-        ),
+        ({ tags }) => allTagsSelected(selectedTags, tags),
       ),
-    [selectedCategory, selectedTags],
+    [selectedTags],
   );
 
   const tags = useMemo(
@@ -109,27 +56,6 @@ const CategorizedInterfaceList = ({
   return (
     <div className="new-stage-screen__categorized">
       <div className="new-stage-screen__menu">
-        <div className="new-stage-screen__menu-section">
-          <motion.ul className="new-stage-screen__menu-items">
-            <MenuItem
-              onClick={handleChangeCategory}
-              category="all"
-              selected={selectedCategory === undefined}
-            >
-              All
-            </MenuItem>
-            { Object.values(CATEGORIES).map((category) => (
-              <MenuItem
-                key={category}
-                onClick={handleChangeCategory}
-                category={category}
-                selected={selectedCategory === category}
-              >
-                {category}
-              </MenuItem>
-            )) }
-          </motion.ul>
-        </div>
         <div className="new-stage-screen__menu-section">
           <p><strong>Filters:</strong></p>
           <motion.div className="new-stage-screen__menu-tags" layout>
