a0f60cc0b086dc28b48852ba4d18db4643553a68
Joshua Melville <joshua.melville@northwestern.edu>
2023-06-09 01:00:26 +0200

    Fixes not able to close stage editor when filter disabled
    Redux form still validates fields that are not shown, even when their
    value is null. We set fields to invisible with a value of null when they
    are contained within a toggleable section, in order to "disable" them.
    
    Redux form has no concept of disabling a field.
    
    In this case, the hidden field representing the node panel filter was
    still being validated, and the validation function was returning a
    string indicating that the field was empty (because we set it to null in
    order to reset it!). This error was hidden from the user, because the
    field was hidden and deregistered.
    
    The fix is to change the validation function to return undefined if the
    value is null. Could also have been implemented as passing undefined as
    the validation prop to the field based on if the toggleable section is
    open or closed.

--- src/components/Query/ruleValidator.js
+++ src/components/Query/ruleValidator.js
@@ -1,14 +1,21 @@
 import { get } from 'lodash';
 
 const validateRules = (value) => {
-  const rules = get(value, 'rules', []);
+  // BUGFIX: If the section containing the filter is not expanded, we set
+  // the filter value to null. In this case, we don't want to
+  // validate the filter, because it will be invisible and will simply
+  // prevent the form from being submitted without an error.
+  if (!value) {
+    return undefined;
+  }
+  const rules = get(value, 'rules');
   const join = get(value, 'join');
 
-  if (rules.length > 1 && !join) {
+  if (rules && rules.length > 1 && !join) {
     return 'Please select a join type';
   }
 
-  if (rules.length === 0) {
+  if (rules && rules.length === 0) {
     return 'Please create at least one rule';
   }
 
