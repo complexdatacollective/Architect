fc7893ad6fbf4cbc96fc1c322982ccca322a2cac
buckhalt <buckhalt@northwestern.edu>
2025-05-09 10:58:46 -0700

    basic expandable sections with checkbox components

--- src/components/sections/Anonymisation/EncryptedVariables.js
+++ src/components/sections/Anonymisation/EncryptedVariables.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Section, Row } from '@components/EditorLayout';
 import { getNodeTypes } from '@selectors/codebook';
-// import { SimpleVariablePill } from '@components/Form/Fields/VariablePicker/VariablePill';
-import { MarkdownLabel } from '@codaco/ui/lib/components/Fields';
+import * as Fields from '@codaco/ui/lib/components/Fields';
 import { actionCreators as codebookActions } from '../../../ducks/modules/protocol/codebook';
 
 const EncryptedVariables = () => {
   const dispatch = useDispatch();
   const nodeTypes = useSelector((state) => getNodeTypes(state));
 
+  const [expandedNodeTypes, setExpandedNodeTypes] = useState({});
+
   const handleEncryptionToggle = (variableId, encrypted) => {
     const action = codebookActions.updateVariableByUUID(variableId, { encrypted }, true);
     dispatch(action);
+  };
+
+  const handleNodeTypeToggle = (nodeTypeId) => {
+    setExpandedNodeTypes((prevState) => ({
+      ...prevState,
+      [nodeTypeId]: !prevState[nodeTypeId],
+    }));
   };
 
   return (
@@ -25,27 +33,31 @@ const EncryptedVariables = () => {
       )}
     >
       {Object.entries(nodeTypes).map(([nodeTypeId, nodeType]) => (
-        <div key={nodeTypeId} className="codebook__entity-variables">
-          <h3>
+        <div key={nodeTypeId}>
+          <h3 onClick={() => handleNodeTypeToggle(nodeTypeId)}>
             {nodeType.name}
           </h3>
 
-          {Object.entries(nodeType.variables || {}).map(([variableId, variable]) => {
-            // Skip layout variables
-            if (variable.type === 'layout') return null;
+          {expandedNodeTypes[nodeTypeId] && (
+            <div>
+              {Object.entries(nodeType.variables || {}).map(([variableId, variable]) => {
+                // Skip layout variables
+                if (variable.type === 'layout') return null;
 
-            return (
-              <Row key={variableId}>
-                <input
-                  type="checkbox"
-                  checked={variable.encrypted || false}
-                  onChange={() => handleEncryptionToggle(variableId, !variable.encrypted)}
-                />
-
-                <MarkdownLabel label={variable.name} />
-              </Row>
-            );
-          })}
+                return (
+                  <Row key={variableId}>
+                    <Fields.Checkbox
+                      label={variable.name}
+                      input={{
+                        value: variable.encrypted || false,
+                        onChange: () => handleEncryptionToggle(variableId, !variable.encrypted),
+                      }}
+                    />
+                  </Row>
+                );
+              })}
+            </div>
+          )}
         </div>
       ))}
     </Section>
